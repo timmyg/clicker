b40577a30a2624c0c3feaa1d905b346f9e55ef79
Tim <timmyg13@gmail.com>
2019-01-04 09:02:47 -0500

    update logzio [skip ci]

--- packages/yacht/app/logger.js
+++ packages/yacht/app/logger.js
@@ -1,8 +1,20 @@
 const LogzioWinstonTransport = require('winston-logzio');
 const winston = require('winston');
 
+const addAppNameFormat = winston.format(info => {
+  info.losantId = process.env.LOSANT_DEVICE_ID;
+  return info;
+});
+
 const logzioWinstonTransport = new LogzioWinstonTransport({
+  format: winston.format.combine(addAppNameFormat(), winston.format.json()),
   level: 'info',
+  // rewriters: [
+  //   (level, msg, meta) => {
+  //     meta.app = process.env;
+  //     return meta;
+  //   },
+  // ],
   token: process.env.LOGZIO_TOKEN,
 });
 const logger = winston.createLogger({
--- packages/yacht/app/startup.js
+++ packages/yacht/app/startup.js
@@ -1,4 +1,7 @@
+const logger = require('./logger');
 require('dotenv').config();
 const Widget = require('./widget');
+
+logger.info('starting up');
 
 (() => new Widget(process.env.LOSANT_DEVICE_ID, process.env.LOSANT_KEY, process.env.LOSANT_SECRET))();
