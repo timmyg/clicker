2bbf63f10edc6a2943c02c992c2d8eb938cd1b79
Tim <timmyg13@gmail.com>
2019-01-30 13:26:22 -0500

    stuff

--- web/app/src/components/landing/Interested.vue
+++ web/app/src/components/landing/Interested.vue
@@ -44,7 +44,7 @@ export default {
     onSubmit() {
       this.submitting = true;
       const { email } = this;
-      this.$ma.setUsername(email);
+      this.$ma.setUsername(email); 
       this.$http
         .post('hitmeback', { email })
         .then(() => {
