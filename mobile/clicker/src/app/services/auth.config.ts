d5fc64f73c7d382016c29ef5d456bb3e712810b1
Tim <timmyg13@gmail.com>
2019-02-04 19:05:37 -0500

    hello world

--- mobile/clicker/src/app/services/auth.config.ts
+++
@@ -1,8 +0,0 @@
-export const AUTH_CONFIG = {
-  // Needed for Auth0 (capitalization: ID):
-  clientID: 'YOUR_CLIENT_ID',
-  // Needed for Auth0Cordova (capitalization: Id):
-  clientId: 'YOUR_CLIENT_ID',
-  domain: 'YOUR_AUTH0_DOMAIN',
-  packageIdentifier: 'YOUR_PACKAGE_ID', // config.xml widget ID, e.g., com.auth0.ionic
-};
---
+++ mobile/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'reserve', loadChildren: './reserve/reserve.module#ReservePageModule' },
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
