ce72d70154884dc0ec25cfe7f537d4a55214d9ec
Tim <timmyg13@gmail.com>
2019-02-16 09:23:26 -0500

    fix

--- web/app/src/components/landing/Hero.vue
+++ web/app/src/components/landing/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="flex-grid">
+    <div class="flex-grid wrapper">
       <div class="hero-copy col">
         <h1 class="listen-up">Change the game</h1>
         <p class="hero-paragraph">Empower your guests to change the channel simply from their phone.</p>
@@ -26,6 +26,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+  flex-direction: column;
+}
 .hero-illustration {
   display: flex;
   justify-content: space-between;
--- web/app/src/components/landing/HeroImage.vue
+++ web/app/src/components/landing/HeroImage.vue
@@ -3,119 +3,137 @@
     <div id="bricks">
       <Bricks />
     </div>
-    <div
-      id="tv-1"
-      class="tv"
-    >
-      <Logo />
+    <div id="tv-1" class="tv">
+      <TV />
+      <div class="channel">
+        <ChannelBaseball />
+        <ChannelBasketball />
+        <ChannelSoccer />
+        <ChannelTennis />
+        <ChannelTrophy />
+        <div class="normal"></div>
+        <div class="tv-static"></div>
+      </div>
     </div>
-    <div
-      id="tv-2"
-      class="tv"
-    >
-      <Logo />
-    </div>
-    <div
-      id="channel-1"
-      class="channel"
-    >
-      <ChannelBaseball />
-      <ChannelBasketball />
-      <ChannelSoccer />
-      <ChannelTennis />
-      <ChannelTrophy />
-    </div>
-    <div
-      id="channel-2"
-      class="channel"
-    >
-      <ChannelBaseball />
-      <ChannelBasketball />
-      <ChannelSoccer />
-      <ChannelTennis />
-      <ChannelTrophy />
+    <div id="tv-2" class="tv">
+      <TV />
+      <div class="channel">
+        <ChannelBaseball />
+        <ChannelBasketball />
+        <ChannelSoccer />
+        <ChannelTennis />
+        <ChannelTrophy />
+        <div class="normal"></div>
+        <div class="tv-static"></div>
+      </div>
     </div>
     <div id="phone">
       <Phone />
     </div>
-    <div
-      id="screen-1"
-      class="screen tv-static "
-    ></div>
-    <div
-      id="screen-1"
-      class="screen tv-static "
-    ></div>
   </section>
 </template>
 
 <script>
-import Bricks from '../../assets/img/hero/wall.svg';
-import Logo from '../../assets/img/hero/tv-mounted.svg';
-import Phone from '../../assets/img/hero/phone-hand.svg';
-import ChannelBaseball from '../../assets/img/hero/channels/baseball.svg';
-import ChannelBasketball from '../../assets/img/hero/channels/basketball.svg';
-import ChannelSoccer from '../../assets/img/hero/channels/soccer.svg';
-import ChannelTennis from '../../assets/img/hero/channels/tennis.svg';
-import ChannelTrophy from '../../assets/img/hero/channels/trophy.svg';
-export default {
-  name: 'HeroImage',
-  components: {
-    Bricks,
-    Logo,
-    Phone,
-    ChannelBaseball,
-    ChannelBasketball,
-    ChannelSoccer,
-    ChannelTennis,
-    ChannelTrophy,
-  },
-};
+    import Bricks from '../../assets/img/hero/wall.svg';
+    import TV from '../../assets/img/hero/tv-mounted.svg';
+    import Phone from '../../assets/img/hero/phone-hand.svg';
+    import ChannelBaseball from '../../assets/img/hero/channels/baseball.svg';
+    import ChannelBasketball from '../../assets/img/hero/channels/basketball.svg';
+    import ChannelSoccer from '../../assets/img/hero/channels/soccer.svg';
+    import ChannelTennis from '../../assets/img/hero/channels/tennis.svg';
+    import ChannelTrophy from '../../assets/img/hero/channels/trophy.svg';
+    export default {
+        name: 'HeroImage',
+        components: {
+            Bricks,
+            TV,
+            Phone,
+            ChannelBaseball,
+            ChannelBasketball,
+            ChannelSoccer,
+            ChannelTennis,
+            ChannelTrophy,
+        },
+    };
 </script>
 
 <style lang="scss" scoped>
-@import '../../assets/scss/custom/static';
-.tv {
-  position: absolute;
-  top: -30px;
-  svg {
-    height: 200px;
-  }
-  &#tv-1 {
-    left: -40px;
-  }
-  &#tv-2 {
-    left: 220px;
-  }
-}
-#phone {
-  position: absolute;
-  bottom: -110px;
-  svg {
-    height: 200px;
-  }
-}
-.channel {
-  top: -30px;
-  svg {
-    height: 60px;
-    position: absolute;
-    top: 40px;
-  }
-  &#channel-1 {
-    svg {
-      left: -40px;
+    @import '../../assets/scss/custom/static';
+    .tv {
+        position: absolute;
+        top: -30px;
+        svg {
+            height: 200px;
+        }
+        &#tv-1 {
+            left: -130px;
+            .channel {
+                left: 196px;
+                svg {
+                    left: -193px;;
+                }
+            }
+        }
+        &#tv-2 {
+            left: 170px;
+            .channel {
+                left: 196px;
+                svg {
+                    left: 93px;
+                }
+            }
+        }
+        .channel {
+            top: 65px;
+            height: 74px;
+            width: 135px;
+            border-radius: 40px;
+            position: absolute;
+            top: 34px;
+            .normal,
+            .tv-static {
+                position: absolute;
+                height: inherit;
+                width: inherit;
+                border-radius: 2px;
+            }
+            .normal {
+                background: white;
+            }
+            svg {
+                position: absolute;
+                height: 100px;
+            }
+        }
     }
-  }
-  &#channel-2 {
-    svg {
-      left: 220px;
+    
+    #phone {
+        position: absolute;
+        bottom: 5px;
+        left: 30px;
+        svg {
+            height: 100px;
+        }
     }
-  }
-}
-.screen {
-  height: 50px;
-  width: 100px;
-}
-</style>
+    
+    
+    // .screen {
 
+        // &#screen-1 {
+        //     left: 67px;
+        // }
+        // &#screen-2 {
+        //     left: 367px;
+        // }
+        // .normal,
+        // .tv-static {
+        //     position: absolute;
+        //     height: inherit;
+        //     width: inherit;
+        //     border-radius: 2px;
+        // }
+        // .normal {
+        //     background: white;
+        // }
+    // }
+</style>
