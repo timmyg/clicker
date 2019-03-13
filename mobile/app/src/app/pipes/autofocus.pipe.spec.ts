1b29ea5df981705b07e0ce4b82b1163461c44fff
Tim <tim.giblin.contractor@8451.com>
2019-03-12 21:43:49 -0400

    modify menu

--- mobile/app/src/app/pipes/autofocus.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AutofocusPipe } from './autofocus.pipe';
-
-describe('AutofocusPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AutofocusPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
