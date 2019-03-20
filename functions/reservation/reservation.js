e575100881e78a6f9948941051e8caefd6191504
Tim <tim.giblin.contractor@8451.com>
2019-03-20 16:07:03 -0400

    add dynamo tables

--- functions/reservation/reservation.js
+++ functions/reservation/reservation.js
@@ -3,3 +3,41 @@ const { respond } = require('serverless-helpers');
 module.exports.health = async event => {
   return respond(200, `hello`);
 };
+
+// TODO
+module.exports.getAll = async event => {
+  // get user
+  // get all reservations for a user
+  return respond(200, `hello`);
+};
+
+// TODO
+module.exports.create = async event => {
+  // get user
+  // get from body (location, program, box, cost)
+  // create reservation
+  // change channel
+  return respond(200, `hello`);
+};
+
+// TODO
+module.exports.cancel = async event => {
+  // get reservation id from path
+  // update reservation to cancelled
+  return respond(200, `hello`);
+};
+
+// TODO
+module.exports.changeChannel = async event => {
+  // get reservation id from path
+  // update reservation channel
+  // change channel
+  return respond(200, `hello`);
+};
+
+// TODO
+module.exports.changeTime = async event => {
+  // get reservation id from path
+  // update reservation end time
+  return respond(200, `hello`);
+};
