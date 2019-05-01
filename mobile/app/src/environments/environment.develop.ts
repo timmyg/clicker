d77f856eaaefbb5dfcc8ab5f9e2810ebaf2dd49a
Tim <tim.giblin.contractor@8451.com>
2019-05-01 19:35:10 -0400

    fix build

--- mobile/app/src/environments/environment.develop.ts
+++ mobile/app/src/environments/environment.develop.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'https://api-dev.tryclicker.com',
+  auth0: {
+    domain: 'clikr.auth0.com',
+    clientId: 'qwIw7MegUSDz8MDk5UVIQcTSStW5xLNt',
+  },
 };
