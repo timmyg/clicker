5a6a26f7756530fa78d86050e9069cbb2fc8e000
Tim <tim.giblin.contractor@8451.com>
2019-05-08 12:28:29 -0400

    env

--- mobile/app/src/environments/environment.develop.ts
+++ mobile/app/src/environments/environment.develop.ts
@@ -3,9 +3,9 @@ import { commonEnvironment } from './environment.common';
 export const environment = {
   ...commonEnvironment,
   production: false,
-  apiBaseUrl: 'https://api-dev.tryclicker.com',
-  auth0: {
-    domain: 'clikr-develop.auth0.com',
-    clientId: 'abgmbT4pqRZROXxn383G4Js0dMMP4bRY',
-  },
+  // apiBaseUrl: 'https://api-dev.tryclicker.com',
+  // auth0: {
+  //   domain: 'clikr-develop.auth0.com',
+  //   clientId: 'abgmbT4pqRZROXxn383G4Js0dMMP4bRY',
+  // },
 };
