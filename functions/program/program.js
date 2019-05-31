dc895bac441164e478888eec99664656e901718f
Tim <tim.giblin.contractor@8451.com>
2019-05-31 15:41:19 -0400

    fs2, less prod throughput

--- functions/program/program.js
+++ functions/program/program.js
@@ -16,6 +16,7 @@ const allChannels = [
   { channel: 209, channelTitle: 'ESPN2' },
   { channel: 213, channelTitle: 'MLB' },
   { channel: 219, channelTitle: 'FS1' },
+  { channel: 618, channelTitle: 'FS2' },
   { channel: 245, channelTitle: 'TNT' },
   { channel: 247, channelTitle: 'TBS' },
   { channel: 220, channelTitle: 'NBCSN' },
