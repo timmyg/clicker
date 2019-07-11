46cf0581833f72786c211f318cefa6c58055538e
Tim <tim.giblin.contractor@8451.com>
2019-07-11 10:53:44 -0400

    tokens

--- functions/app/app.js
+++ functions/app/app.js
@@ -3,14 +3,13 @@ const { respond } = require('serverless-helpers');
 const plans = [
   { tokens: 2, dollars: 10, id: 'bb23310b-3fec-4873-8cdf-279ffcd31b32' },
   { tokens: 5, dollars: 20, id: '40167d6a-1626-456a-8a35-f60c91c05120' },
-  { tokens: 8, dollars: 25, id: 'd25f930b-7c3a-4ea2-b41b-9ae959135c6a' },
+  { tokens: 10, dollars: 30, id: 'd25f930b-7c3a-4ea2-b41b-9ae959135c6a', best: true },
 ];
 
 module.exports.health = async event => {
   return respond(200, `hello`);
 };
 
-module.exports.tokenPlans = async event => {
+module.exports.tokens = async event => {
   return respond(200, plans);
-  
 };
