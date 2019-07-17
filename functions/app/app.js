08d0ca8d4d2b4b1516134fbe9da9fc560eb49a34
Tim <tim.giblin.contractor@8451.com>
2019-07-17 10:22:11 -0400

    costs

--- functions/app/app.js
+++ functions/app/app.js
@@ -5,6 +5,7 @@ const plans = [
   { tokens: 5, dollars: 20, id: '40167d6a-1626-456a-8a35-f60c91c05120' },
   { tokens: 10, dollars: 30, id: 'd25f930b-7c3a-4ea2-b41b-9ae959135c6a', best: true },
 ];
+const costs = [{ tokens: 1, minutes: 30 }, { tokens: 2, minutes: 60 }];
 
 module.exports.health = async event => {
   return respond(200, `hello`);
@@ -12,4 +13,8 @@ module.exports.health = async event => {
 
 module.exports.buy = async event => {
   return respond(200, plans);
+};
+
+module.exports.costs = async event => {
+  return respond(200, costs);
 };
