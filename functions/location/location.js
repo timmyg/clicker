ad5b50d96423c9f74c10cbe202393ece309da75e
Tim <tim.giblin.contractor@8451.com>
2019-07-18 12:07:53 -0400

    fixes

--- functions/location/location.js
+++ functions/location/location.js
@@ -92,7 +92,9 @@ module.exports.getLocalChannels = async event => {
   allLocations.forEach(l => {
     locationsByZip[l.zip] = locationsByZip[l.zip] || [];
     console.log(l.zip, locationsByZip[l.zip]);
-    locationsByZip[l.zip] = locationsByZip[l.zip].concat(l.channels.local);
+    if (l.channels && l.channels.local) {
+      locationsByZip[l.zip] = locationsByZip[l.zip].concat(l.channels.local);
+    }
     // remove duplicates
     locationsByZip[l.zip] = locationsByZip[l.zip].filter((elem, index, self) => {
       return index === self.indexOf(elem);
