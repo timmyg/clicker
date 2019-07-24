d1ff7983cef0cdd9b9497018c5766ffb02a6cac6
tim <timmyg13@gmail.com>
2019-07-24 17:05:16 -0400

    add another timeframe

--- functions/app/app.js
+++ functions/app/app.js
@@ -5,7 +5,7 @@ const plans = [
   { tokens: 5, dollars: 20, id: '40167d6a-1626-456a-8a35-f60c91c05120' },
   { tokens: 10, dollars: 30, id: 'd25f930b-7c3a-4ea2-b41b-9ae959135c6a', best: true },
 ];
-const timeframes = [{ tokens: 1, minutes: 30 }, { tokens: 2, minutes: 60 }];
+const timeframes = [{ tokens: 1, minutes: 30 }, { tokens: 2, minutes: 60 }, { tokens: 3, minutes: 120 }];
 
 module.exports.health = async event => {
   return respond(200, `hello`);
