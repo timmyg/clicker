984a50d77a6371b0b7c9a6dffa36f17b90d14caf
tim <timmyg13@gmail.com>
2019-07-24 17:36:24 -0400

    fixes

--- functions/app/app.js
+++ functions/app/app.js
@@ -5,7 +5,7 @@ const plans = [
   { tokens: 5, dollars: 20, id: '40167d6a-1626-456a-8a35-f60c91c05120' },
   { tokens: 10, dollars: 30, id: 'd25f930b-7c3a-4ea2-b41b-9ae959135c6a', best: true },
 ];
-const timeframes = [{ tokens: 1, minutes: 30 }, { tokens: 2, minutes: 60 }, { tokens: 3, minutes: 120 }];
+const timeframes = [{ tokens: 1, minutes: 30 }, { tokens: 2, minutes: 60 }];
 
 module.exports.health = async event => {
   return respond(200, `hello`);
@@ -17,5 +17,6 @@ module.exports.buy = async event => {
 
 module.exports.timeframes = async event => {
   console.log(event);
+  // locationId should be available in query params
   return respond(200, timeframes);
 };
