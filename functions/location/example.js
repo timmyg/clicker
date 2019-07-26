6d91a7e548791d97705fe278dc2e1115bc6a3f1d
tim <timmyg13@gmail.com>
2019-07-26 18:01:00 -0400

    game start

--- functions/location/example.js
+++ functions/location/example.js
@@ -6,7 +6,7 @@
   console.log(process.env.airtableBase);
   const base = new Airtable({ apiKey: process.env.airtableKey }).base(process.env.airtableBase);
   const games = await base('Games')
-    .select({ view: 'Ready To Change' })
+    .select({ view: 'Scheduled', filterByFormula: '{Game Start} <= NOW()' })
     .all();
   console.log(games.length);
   console.log(games[0].get('Game Start'));
