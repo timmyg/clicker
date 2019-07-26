bf9009834aabce9a4828e05cbac30715d1f0a1cc
tim <timmyg13@gmail.com>
2019-07-26 18:48:47 -0400

    fix date formula

--- functions/location/example.js
+++ functions/location/example.js
@@ -1,13 +1,14 @@
 (async () => {
-  console.log('at');
   const Airtable = require('airtable');
-
   console.log(process.env.airtableKey);
   console.log(process.env.airtableBase);
   const base = new Airtable({ apiKey: process.env.airtableKey }).base(process.env.airtableBase);
   const games = await base('Games')
-    .select({ view: 'Scheduled', filterByFormula: '{Game Start} < TIMESTR(NOW())' })
+    // .select({ view: 'Scheduled', filterByFormula: `DATESTR({Game Start}) <= DATESTR(NOW())` })
+    .select({ view: 'Scheduled', filterByFormula: `DATETIME_DIFF({Game Start}, NOW(), 'milliseconds') < 0` })
     .all();
   console.log(games.length);
-  console.log(games[0].get('Game Start'));
+  if (games.length) {
+    console.log(games[0].get('Game Start'));
+  }
 })();
