f2c2627701bb48832471a9faa6a66595d840ca37
tim <timmyg13@gmail.com>
2019-07-26 18:21:13 -0400

    timestring

--- functions/location/example.js
+++ functions/location/example.js
@@ -6,7 +6,7 @@
   console.log(process.env.airtableBase);
   const base = new Airtable({ apiKey: process.env.airtableKey }).base(process.env.airtableBase);
   const games = await base('Games')
-    .select({ view: 'Scheduled', filterByFormula: '{Game Start} < NOW()' })
+    .select({ view: 'Scheduled', filterByFormula: '{Game Start} < TIMESTR(NOW())' })
     .all();
   console.log(games.length);
   console.log(games[0].get('Game Start'));
