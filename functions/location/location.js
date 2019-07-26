7d2a3ebb322a1039fa6949be573112a32806d6cd
tim <timmyg13@gmail.com>
2019-07-26 17:11:30 -0400

    view name

--- functions/location/location.js
+++ functions/location/location.js
@@ -349,7 +349,7 @@ module.exports.controlCenter = async event => {
   const base = new Airtable({ apiKey: process.env.airtableKey }).base(process.env.airtableBase);
   console.log('searching for games to change');
   const games = await base('Games')
-    .select({ view: 'Ready To Change' })
+    .select({ view: 'Changing...' })
     .all();
   console.log(`found ${games.length} games`);
   console.log(games);
