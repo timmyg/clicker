048261d663c3bd1c53bf74d2b7ef1d5bc0d91d84
Tim <tim.giblin.contractor@8451.com>
2019-08-26 14:23:09 -0400

    add appflow check in script

--- appflow.sh
+++ appflow.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "Moving mobile app to root so that Appflow works"
-mv -v ./mobile/app/* ./
+echo "running appflow.sh . . .";
+if [ "$CI_APP_ID" = "aab910fb" ] # ionic appflow app id
+then
+    echo "Moving mobile app to root so that Appflow works";
+    echo "Appflow build";
+    mv -v ./mobile/app/* ./
+else 
+    echo "Not an Appflow build"; 
+fi
