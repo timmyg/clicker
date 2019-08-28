72fffbd312fd3b5302eb9b150376c50f9ad69a75
Tim <tim.giblin.contractor@8451.com>
2019-08-28 11:36:52 -0400

    fix missing programs

--- functions/program/program.js
+++ functions/program/program.js
@@ -26,7 +26,6 @@ const nationalChannels = [
   { channel: 602, channelTitle: 'TVG' },
   { channel: 612, channelTitle: 'ACCN' },
   { channel: 618, channelTitle: 'FS2' },
-  { channel: 620, channelTitle: 'FS2' },
   { channel: 610, channelTitle: 'BTN' },
   { channel: 611, channelTitle: 'SECHD' },
   { channel: 605, channelTitle: 'SPMN' },
@@ -570,8 +569,8 @@ function build(dtvSchedule, zip, channels) {
         program.live = program.ltd === 'Live' ? true : false;
         program.repeat = program.repeat;
         program.zip = zip;
-        program.id = generateId(program);
         program.start = new Date(parseInt(moment(program.airTime).unix() * 1000));
+        program.id = generateId(program);
         program.end = new Date(
           parseInt(
             moment(program.airTime)
@@ -589,8 +588,8 @@ function build(dtvSchedule, zip, channels) {
 }
 
 function generateId(program) {
-  const { programId, zip } = program;
-  const id = programId + (zip || '');
+  const { programId, zip, start } = program;
+  const id = programId + start + (zip || '');
   // console.log(id, uuid.DNS);
   return uuid(id, uuid.DNS);
 }
