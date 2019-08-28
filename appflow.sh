d39d3df09fba4881fd4af0ff9821d46dee252215
tim <timmyg13@gmail.com>
2019-08-27 21:56:40 -0400

    remove everything but mobile

--- appflow.sh
+++ appflow.sh
@@ -3,7 +3,9 @@ echo "running appflow.sh . . .";
 if [ "$CI_APP_ID" = "aab910fb" ] # ionic appflow app id
 then
     echo "Moving mobile app to root so that Appflow works";
-    echo "Appflow build";
+    echo "remove everything except mobile/app directory";
+    find * -maxdepth 0 -name 'mobile' -prune -o -exec rm -rf '{}' ';'
+    echo "move to root";
     mv -v ./mobile/app/* ./
 else 
     echo "Not an Appflow build"; 
