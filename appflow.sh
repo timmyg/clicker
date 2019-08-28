41b3e20e9246eb4224818acec13be72eb1e61878
tim <timmyg13@gmail.com>
2019-08-28 08:15:11 -0400

    appflow script remove explicitly

--- appflow.sh
+++ appflow.sh
@@ -4,7 +4,16 @@ if [ "$CI_APP_ID" = "aab910fb" ] # ionic appflow app id
 then
     echo "Moving mobile app to root so that Appflow works";
     echo "remove everything except mobile/app directory";
-    find * -maxdepth 0 -name 'mobile' -prune -o -exec rm -rf '{}' ';'
+    # find * -maxdepth 0 -name 'mobile' -prune -o -exec rm -rf '{}' ';'
+    rm -rf README.md
+    rm -rf docs
+    rm -rf packages
+    rm -rf setup-complete.txt
+    rm -rf web
+    rm -rf node_modules
+    rm -rf package-lock.json
+    rm -rf functions
+    rm -rf netlify.toml
     echo "move to root";
     mv -v ./mobile/app/* ./
     echo "ls:";
