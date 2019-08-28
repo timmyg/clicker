b1780abefb2620e8e2a66ad3907aa3cfca1e50c8
Tim <tim.giblin.contractor@8451.com>
2019-08-28 15:04:30 -0400

    wip

--- web/blog/pages/blog.vue
+++ web/blog/pages/blog.vue
@@ -5,10 +5,36 @@
         <p class="subtitle is-6">
           <nuxt-link :to="{name :'blog'}">Back to Blog home</nuxt-link>
         </p>
-        <h1 class="title is-2">{{ $route.params.post.title }}</h1>
+        <h1 class="title is-2">{{ post.title }}</h1>
         <hr />
-        <div class="content">{{$route.params.post.content}}</div>
+        <div class="content">{{post.content}}</div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import { createClient } from '~/plugins/contentful.js';
+
+const client = createClient();
+export default {
+  //   data() {
+  //   return {
+  //     post: null,
+  //   };
+  // },
+  asyncData({ env }) {
+    const { slug } = this.$route.params;
+    return Promise.all([
+      client.getEntries({
+        content_type: 'blogPost',
+        'fields.slug': slug,
+      }),
+    ]).then(([posts]) => {
+      return {
+        post: posts.items[0],
+      };
+    });
+  },
+};
+</script>
--- web/blog/pages/index.vue
+++ web/blog/pages/index.vue
@@ -6,7 +6,7 @@
         <hr />
         <h2 class="title is-4" v-for="(post, index) in posts" :key="index">
           <nuxt-link
-            :to="{name : 'blogpost',params: { post : post.fields.slug}}"
+            :to="{name : 'blog',params: { post : post.fields.slug}}"
           >{{ post.fields.title }}</nuxt-link>
         </h2>
       </div>
@@ -19,26 +19,17 @@ import { createClient } from '~/plugins/contentful.js';
 
 const client = createClient();
 export default {
-  // `env` is available in the context object
   asyncData({ env }) {
     return Promise.all([
-      // fetch the owner of the blog
-      // client.getEntries({
-      //   'sys.id': env.CTF_PERSON_ID
-      // }),
-      // fetch all blog posts sorted by creation date
       client.getEntries({
         content_type: 'blogPost',
         order: '-sys.createdAt',
       }),
     ]).then(([posts]) => {
-      // return data that should be available
-      // in the template
       return {
         posts: posts.items,
       };
     });
-    // .catch(.error)
   },
 };
 </script>
