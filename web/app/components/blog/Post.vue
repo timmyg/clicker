be0ef0a334c1bbf57c36422fe9e74768cea91d16
Tim <tim.giblin.contractor@8451.com>
2019-09-03 15:10:47 -0400

    nuxt site w/ blog

---
+++ web/app/components/blog/Post.vue
@@ -0,0 +1,24 @@
+<template>
+  <article>
+    <!-- <div class="img-wrapper"> -->
+    <img :src="post.fields.featuredImage.fields.file.url + '?fit=fill&w=1500&h=450&f=center'" />
+    <!-- </div> -->
+    <h1 class="title is-2">{{ post.fields.title }}</h1>
+    <hr />
+    <div v-html="$md.render(post.fields.content)"></div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ['post'],
+};
+</script>
+
+<style lang="scss" scoped>
+// .img-wrapper {
+//   img {
+//     height: 300px;
+//   }
+// }
+</style>
---
+++ web/app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Landing />
+  </div>
+</template>
+
+<script>
+import Landing from '~/components/landing/Landing';
+
+export default {
+  components: {
+    Landing,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
