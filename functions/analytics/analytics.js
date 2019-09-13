9dc056f08794ab0523f731500a208ef50a36895f
Tim <tim.giblin.contractor@8451.com>
2019-09-13 11:22:02 -0400

    debuggin

--- functions/analytics/analytics.js
+++ functions/analytics/analytics.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const { respond, getBody, track } = require('serverless-helpers');
 
 module.exports.track = async event => {
+  console.log('1.');
   const { userId, name, data } = getBody(event);
+  console.log('2.');
   await track({ userId, event: name, properties: data });
+  console.log('3.');
   return respond(200);
 };
 
 module.exports.health = async event => {
   return respond(200, `hello`);
 };
- 
