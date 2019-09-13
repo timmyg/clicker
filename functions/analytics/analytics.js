3b589c1ceca22218e6c7d870d4f9d6143ec1dc5f
Tim <tim.giblin.contractor@8451.com>
2019-09-13 13:30:09 -0400

    analytics timeout

--- functions/analytics/analytics.js
+++ functions/analytics/analytics.js
@@ -3,6 +3,7 @@ const { respond, getBody, track } = require('serverless-helpers');
 
 module.exports.track = async event => {
   const { userId, name, data } = getBody(event);
+  console.log({ userId, event: name, properties: data });
   await track({ userId, event: name, properties: data });
   return respond(200);
 };
