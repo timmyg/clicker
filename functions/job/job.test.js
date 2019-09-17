ce8e2a7d36fbfc0ec8dbd905765a9b161ef2ac8b
Tim <tim.giblin.contractor@8451.com>
2019-09-17 11:18:31 -0400

    fix

--- functions/job/job.test.js
+++ functions/job/job.test.js
@@ -8,8 +8,10 @@ test('smoke test', () => {
 
 test('get initial channels', () => {
   expect(getChannelForZone(0)).toBe(206);
-  expect(getChannelForZone(1)).toBe(207);
-  expect(getChannelForZone(2)).toBe(209);
-  expect(getChannelForZone(3)).toBe(206);
-  expect(getChannelForZone(4)).toBe(207);
+  expect(getChannelForZone(1)).toBe(209);
+  expect(getChannelForZone(2)).toBe(614);
+  expect(getChannelForZone(3)).toBe(208);
+  expect(getChannelForZone(4)).toBe(212);
+  expect(getChannelForZone(4)).toBe(210);
+  expect(getChannelForZone(4)).toBe(206);
 });
