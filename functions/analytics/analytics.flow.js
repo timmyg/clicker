bbe9f8c6f9bd3736ebf64dcfacde69dd1216befa
Tim <tim.giblin.contractor@8451.com>
2019-10-01 14:51:50 -0400

    analytics base path

--- functions/analytics/analytics.flow.js
+++ functions/analytics/analytics.flow.js
@@ -2,12 +2,10 @@
 require('dotenv').config();
 const axios = require('axios');
 const { respond, getBody } = require('serverless-helpers');
-const analytics = new (require('analytics-node'))(process.env.segmentWriteKey || process.env.SEGMENT_WRITE_KEY);
+const analytics = new (require('analytics-node'))(process.env.segmentWriteKey);
 
 module.exports.track = async (event: any) => {
   const { userId, name, data } = getBody(event);
-  // console.log({ userId, event: name, properties: data });
-  // await track({ userId, event: name, properties: data });
   await axios.post('https://api.segment.io/v1/track', { userId, event: name, properties: data });
   return respond(200);
 };
