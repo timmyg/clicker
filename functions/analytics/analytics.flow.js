5e36e56334ab8439a46efca517325fc2e8f16724
Tim <tim.giblin.contractor@8451.com>
2019-10-01 15:11:19 -0400

    add date

--- functions/analytics/analytics.flow.js
+++ functions/analytics/analytics.flow.js
@@ -2,10 +2,11 @@
 require('dotenv').config();
 const axios = require('axios');
 const { respond, getBody } = require('serverless-helpers');
-const analytics = new (require('analytics-node'))(process.env.segmentWriteKey);
 
 module.exports.track = async (event: any) => {
   const { userId, name, data } = getBody(event);
+  // add in time
+  data.date = new Date().toISOString();
   const body = { userId, event: name, properties: data };
   const options = {
     auth: {
