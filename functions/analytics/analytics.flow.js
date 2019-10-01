66dd9261605ceedb756743ed5cacec1daeaeb18e
Tim <tim.giblin.contractor@8451.com>
2019-10-01 14:59:52 -0400

    segment auth

--- functions/analytics/analytics.flow.js
+++ functions/analytics/analytics.flow.js
@@ -6,7 +6,13 @@ const analytics = new (require('analytics-node'))(process.env.segmentWriteKey);
 
 module.exports.track = async (event: any) => {
   const { userId, name, data } = getBody(event);
-  await axios.post('https://api.segment.io/v1/track', { userId, event: name, properties: data });
+  const body = { userId, event: name, properties: data };
+  const options = {
+    auth: {
+      username: process.env.segmentWriteKey,
+    },
+  };
+  await axios.post('https://api.segment.io/v1/track', body, options);
   return respond(200);
 };
 
