1e9dc4ce4b5f41950674cfbe11fba97f390739fb
Tim <tim.giblin.contractor@8451.com>
2019-10-01 14:45:17 -0400

    test axios http call rather than segment node

--- functions/analytics/analytics.flow.js
+++ functions/analytics/analytics.flow.js
@@ -1,11 +1,14 @@
 // @flow
 require('dotenv').config();
-const { respond, getBody, track } = require('serverless-helpers');
+const axios = require('axios');
+const { respond, getBody } = require('serverless-helpers');
+const analytics = new (require('analytics-node'))(process.env.segmentWriteKey || process.env.SEGMENT_WRITE_KEY);
 
 module.exports.track = async (event: any) => {
   const { userId, name, data } = getBody(event);
-  console.log({ userId, event: name, properties: data });
-  await track({ userId, event: name, properties: data });
+  // console.log({ userId, event: name, properties: data });
+  // await track({ userId, event: name, properties: data });
+  await axios.post('https://api.segment.io/v1/track', { userId, event: name, properties: data });
   return respond(200);
 };
 
