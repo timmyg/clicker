ebb006539fc279f8e80059ec5eebd6d519264b3a
tim <timmyg13@gmail.com>
2019-10-21 12:32:09 -0400

    airtable typecast

--- functions/admin/admin.flow.js
+++ functions/admin/admin.flow.js
@@ -68,15 +68,18 @@ module.exports.logChannelChange = RavenLambdaWrapper.handler(Raven, async event 
 
   console.time('send to airtable');
   const base = new Airtable({ apiKey: process.env.airtableKey }).base(process.env.airtableBase);
-  await base('Channel Changes').create({
-    Location: location,
-    Zone: zone,
-    From: from,
-    To: to,
-    Time: time,
-    Type: type,
-    'Box Id': boxId,
-  });
+  await base('Channel Changes').create(
+    {
+      Location: location,
+      Zone: zone,
+      From: from,
+      To: to,
+      Time: time,
+      Type: type,
+      'Box Id': boxId,
+    },
+    { typecast: true },
+  );
   console.timeEnd('send to airtable');
 
   return respond(200);
