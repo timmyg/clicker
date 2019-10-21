e49976215c8683ccd1a44d43ba2be79ead9cb13a
Tim <tim.giblin.contractor@8451.com>
2019-10-21 13:36:11 -0400

    epl, la liga, mls

--- functions/game/game.flow.js
+++ functions/game/game.flow.js
@@ -151,9 +151,13 @@ function transformSIUrl(webUrl: string): string {
   let sport = urlParts[3];
   if (sport === 'college-football') {
     sport = 'ncaaf';
+  } else if (['epl', 'mls', 'la liga'].includes(decodeURIComponent(sport))) {
+    apiUrl.push('soccer');
+    apiUrl.push(`game_detail?id=${urlParts[5]}&league=${sport}`);
+  } else {
+    apiUrl.push(sport);
+    apiUrl.push(`game_detail?id=${urlParts[5]}`);
   }
-  apiUrl.push(sport);
-  apiUrl.push(`game_detail?id=${urlParts[5]}`);
   return apiUrl.join('/');
 }
 
