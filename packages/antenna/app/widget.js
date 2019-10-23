b2c09b9e4f27091da70e587e204ae366474dfaed
Tim <tim.giblin.contractor@8451.com>
2019-10-23 12:49:55 -0400

    update antenna dep, logging

--- packages/antenna/app/widget.js
+++ packages/antenna/app/widget.js
@@ -121,6 +121,8 @@ class Widget {
                 const { boxId, client, ip } = box;
                 const _remote = new DirecTV.Remote(ip);
                 logger.info(`get tuned for box: ${boxId}`);
+                logger.info('_remote.getTuned', _remote.getTuned);
+                logger.info('_remote.getTunedSync', _remote.getTunedSync);
                 const info = await _remote.getTunedSync(client || '0');
                 logger.info({ info });
                 boxesInfo.push({ boxId, info });
