ddb5087c92c1e87c85ba46684d66d84088ec5639
tim <timmyg13@gmail.com>
2019-11-01 18:13:26 -0400

    twilio debugging

--- functions/user/user.flow.js
+++ functions/user/user.flow.js
@@ -372,6 +372,7 @@ module.exports.verify = RavenLambdaWrapper.handler(Raven, async event => {
   const client = new twilio(twilioAccountSid, twilioAuthToken);
 
   try {
+    console.log(twilioAccountSid, twilioAuthToken, twilioServiceSid);
     const result = await client.verify.services(twilioServiceSid).verificationChecks.create({ to: phone, code });
     if (result.status === 'approved') {
       const token = await getToken(phone);
