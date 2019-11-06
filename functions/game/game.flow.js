09e59be6b3f5a0283ff02a2f8068b901c19cb04c
tim <timmyg13@gmail.com>
2019-11-05 21:43:44 -0500

    dbevents

--- functions/game/game.flow.js
+++ functions/game/game.flow.js
@@ -304,6 +304,7 @@ async function createAll(events: any[]) {
     };
     try {
       console.log({ params });
+      console.log(JSON.stringify(dbEvents));
       const result = await docClient.batchWrite(params).promise();
       console.log({ result });
     } catch (e) {
