dafef55a9f5e1ffe2041cfb7863e25d15bb3c966
tim <timmyg13@gmail.com>
2019-11-15 17:49:21 -0500

    fix

--- functions/game/game.flow.js
+++ functions/game/game.flow.js
@@ -256,19 +256,19 @@ module.exports.scoreboard = RavenLambdaWrapper.handler(Raven, async event => {
     //   .exec();
     const allGames = await Game.scan().exec();
     console.log(allGames.length);
-    const sortedGames = allGames;
-    // const sortedGames = [];
-    // sortedGames.push(allGames.filter(g => g.status === 'inprogress'));
-    // console.log(1);
-    // sortedGames.push(allGames.filter(g => g.status === 'complete'));
-    // console.log(2);
-    // sortedGames.push(allGames.filter(g => g.status === 'scheduled'));
-    // console.log(3);
-    // sortedGames.push(allGames.filter(g => !['inprogress', 'complete', 'scheduled'].includes(g.status)));
+    // const sortedGames = allGames;
+    const sortedGames = [];
+    sortedGames.push(...allGames.filter(g => g.status === 'inprogress'));
+    console.log(1);
+    sortedGames.push(...allGames.filter(g => g.status === 'complete'));
+    console.log(2);
+    sortedGames.push(...allGames.filter(g => g.status === 'scheduled'));
+    console.log(3);
+    sortedGames.push(...allGames.filter(g => !['inprogress', 'complete', 'scheduled'].includes(g.status)));
 
     console.log(sortedGames.length);
     console.timeEnd('all scores');
-    return respond(200, sortedGames);
+    return respond(200, sortedGames.length);
   } catch (e) {
     console.error(e);
     respond(400, e);
