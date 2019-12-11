6356673d5014c42838d277a94b91f7046327bfc9
Tim <tim.giblin.contractor@8451.com>
2019-12-11 11:30:43 -0500

    remove si dependency

--- functions/game/game.flow.js
+++ functions/game/game.flow.js
@@ -99,44 +99,6 @@ const dbGame = dynamoose.model(
 );
 // }
 
-class SiLeague {
-  name: string; // Major League Baseball, College Football
-  abbreviation: string; // MLB, NCAAF
-}
-
-class SiTeam {
-  title: string; // Iowa State Cyclones
-  abbreviation: string; // Tulane
-  score: number; // 17
-  is_winner: boolean; // false
-}
-
-class SiStatus {
-  period: {
-    id: number, // 4
-    name: string, // 4th
-    unit: string, // Quarter
-    time: string, // 8:04
-  };
-  time: { minutes: number, seconds: number, additionalMinutes: number }; // soccer
-  inning: number; // 1
-  name: string; // Final, Pre-Game
-  is_active: boolean; // false
-  inning_division: string; // "Top"
-}
-
-class SiGame {
-  tv: string;
-  status: SiStatus;
-  teams: SiTeam[];
-  league: SiLeague;
-}
-
-class SiResult {
-  status: string;
-  data: SiGame;
-}
-
 class GameStatus {
   started: boolean;
   blowout: boolean;
@@ -147,36 +109,18 @@ class GameStatus {
 module.exports.getStatus = RavenLambdaWrapper.handler(Raven, async event => {
   console.log('getstatus');
   const { url: webUrl } = getBody(event);
-  if (webUrl.includes('actionnetwork')) {
-    const parts = webUrl.split('/');
-    const gameId = parts[parts.length - 1];
-    const game: Game = await dbGame
-      .queryOne('id')
-      .eq(gameId)
-      .exec();
-    console.log('get game', gameId, game);
-    const status: GameStatus = getStatusV2(game);
-    return respond(200, status);
-  } else {
-    const apiUrl = transformSIUrl(webUrl);
-
-    const method = 'get';
-    const options = { method, url: apiUrl, timeout: 2000 };
-    try {
-      const result = await axios(options);
-      console.log(result.data);
-      const gameStatus: GameStatus = transformGame(result.data);
-
-      return respond(200, gameStatus);
-    } catch (e) {
-      console.error(`failed to get score: ${apiUrl}`);
-      Raven.captureException(e);
-      return respond(400, e);
-    }
-  }
+  const parts = webUrl.split('/');
+  const gameId = parts[parts.length - 1];
+  const game: Game = await dbGame
+    .queryOne('id')
+    .eq(gameId)
+    .exec();
+  console.log('get game', gameId, game);
+  const status: GameStatus = getStatus(game);
+  return respond(200, status);
 });
 
-function getStatusV2(game: Game): GameStatus {
+function getStatus(game: Game): GameStatus {
   const gameStatus = new GameStatus();
   gameStatus.started = ['complete', 'inprogress'].includes(game.status) ? true : false;
   gameStatus.ended = ['complete'].includes(game.status) ? true : false;
@@ -268,107 +212,6 @@ function isBlowout(game: Game): boolean {
     default:
       return true;
   }
-}
-
-function transformGame(result: SiResult): GameStatus {
-  const game = result.data;
-  const gameStatus = new GameStatus();
-  const { name: status } = game.status;
-
-  // set started, ended
-  if (status === 'Final') {
-    gameStatus.started = true;
-    gameStatus.ended = true;
-  } else if (status === 'In-Progress') {
-    gameStatus.started = true;
-    gameStatus.ended = false;
-  } else if (status === 'Pre-Game') {
-    gameStatus.started = false;
-  } else {
-    console.error(`unknown result status: ${status}`);
-  }
-
-  const { abbreviation: leageAbbreviation } = game.league;
-  const [homeTeam, awayTeam] = game.teams; // looks like first team is always home
-  // set blowout
-  if (status === 'In-Progress') {
-    if (leageAbbreviation === 'NCAAF') {
-      // 6:00 left in 4th quarter and 17+ point difference
-      const isNearEnd = game.status.period.id === 4 && +game.status.period.time.split(':')[0] <= 6;
-      const isBlowout = Math.abs(homeTeam.score - awayTeam.score) >= 17;
-      gameStatus.blowout = isNearEnd && isBlowout;
-    } else if (leageAbbreviation === 'NFL') {
-      // 6:00 left in 4th quarter and 17+ point difference
-      const isNearEnd = game.status.period.id === 4 && +game.status.period.time.split(':')[0] <= 6;
-      const isBlowout = Math.abs(homeTeam.score - awayTeam.score) >= 17;
-      gameStatus.blowout = isNearEnd && isBlowout;
-    } else if (leageAbbreviation === 'MLB') {
-      // 8th inning and 5+ run difference
-      const isNearEnd = game.status.inning >= 8;
-      const isBlowout = Math.abs(homeTeam.score - awayTeam.score) >= 5;
-      gameStatus.blowout = isNearEnd && isBlowout;
-    } else if (leageAbbreviation === 'NCAAB') {
-      // 8:00 left in 2nd half and 20+ point difference
-      const isNearEnd = game.status.period.id === 2 && +game.status.period.time.split(':')[0] <= 8;
-      const isBlowout = Math.abs(homeTeam.score - awayTeam.score) >= 20;
-      gameStatus.blowout = isNearEnd && isBlowout;
-    } else if (['NBA', 'WNBA'].includes(leageAbbreviation)) {
-      // 6:00 left in 4th quarter and 25+ point difference
-      const isNearEnd = game.status.period.id === 4 && +game.status.period.time.split(':')[0] <= 6;
-      const isBlowout = Math.abs(homeTeam.score - awayTeam.score) >= 25;
-      gameStatus.blowout = isNearEnd && isBlowout;
-    } else if (['EPL'].includes(leageAbbreviation)) {
-      // 80th minutes and 3+ goal difference
-      const isNearEnd = game.status.period.id === 2 && +game.status.time.minutes >= 80;
-      const isBlowout = Math.abs(homeTeam.score - awayTeam.score) >= 3;
-      gameStatus.blowout = isNearEnd && isBlowout;
-    } else if (['NHL'].includes(leageAbbreviation)) {
-      // 3rd period, 8 minutes and 3+ goal difference
-      const isNearEnd = game.status.period.id === 3 && +game.status.period.time.split(':')[0] <= 8;
-      const isBlowout = Math.abs(homeTeam.score - awayTeam.score) >= 3;
-      gameStatus.blowout = isNearEnd && isBlowout;
-    }
-  }
-
-  // set description
-  gameStatus.description = `${awayTeam.abbreviation} ${awayTeam.score || 0} @ ${
-    homeTeam.abbreviation
-  } ${homeTeam.score || 0}`;
-  if (status === 'In-Progress') {
-    if (['NFL', 'NCAAF', 'NCAAB', 'NBA', 'WNBA', 'NHL'].includes(leageAbbreviation)) {
-      gameStatus.description += ` (${game.status.period.time} ${game.status.period.name})`;
-    } else if (['MLB'].includes(leageAbbreviation)) {
-      gameStatus.description += ` (${game.status.inning_division} ${game.status.inning})`;
-    } else if (['EPL'].includes(leageAbbreviation)) {
-      gameStatus.description += ` (${game.status.time.minutes}:${game.status.time.seconds} ${game.status.period.name})`;
-    }
-  } else if (status === 'Final') {
-    gameStatus.description += ` (Final)`;
-  } else if (status === 'Pre-Game') {
-    gameStatus.description += ` (Pre-Game)`;
-  }
-
-  return gameStatus;
-}
-
-function transformSIUrl(webUrl: string): string {
-  const urlParts = webUrl.split('/');
-  let apiUrl = ['https://stats.api.si.com/v1'];
-  let sport = urlParts[3];
-  if (sport === 'college-football') {
-    sport = 'ncaaf';
-  } else if (sport === 'college-basketball') {
-    sport = 'ncaab';
-  }
-
-  if (['epl', 'mls', 'la-liga', 'serie-a'].includes(decodeURIComponent(sport))) {
-    apiUrl.push('soccer');
-    apiUrl.push(`game_detail?id=${urlParts[5]}&league=${sport}`);
-  } else {
-    apiUrl.push(sport);
-    apiUrl.push(`game_detail?id=${urlParts[5]}`);
-  }
-  return apiUrl.join('/');
 }
 
 module.exports.syncScores = RavenLambdaWrapper.handler(Raven, async event => {
@@ -566,7 +409,7 @@ async function pullFromActionNetwork(dates: Date[]) {
 
 async function updateGames(events: any[]) {
   events.forEach((part, index, eventsArray) => {
-    eventsArray[index] = transformGameV2(eventsArray[index]);
+    eventsArray[index] = transformGame(eventsArray[index]);
   });
   console.log('updateGames:', events.length);
   const { tableGame } = process.env;
@@ -586,7 +429,7 @@ async function updateGames(events: any[]) {
   }
 }
 
-function transformGameV2(game: any): Game {
+function transformGame(game: any): Game {
   // set away, home teams
   game.away = game.teams.find(t => t.id === game.away_team_id);
   game.home = game.teams.find(t => t.id === game.home_team_id);
@@ -599,8 +442,6 @@ function transformGameV2(game: any): Game {
     const homeRank = game.ranks.find(gr => gr.team_id === game.home.id);
     game.home.rank = homeRank ? homeRank.rank : null;
   }
-
-  console.log(game.id);
 
   const map = {
     id: 'id',
@@ -666,8 +507,6 @@ function transformGameV2(game: any): Game {
   return objectMapper(game, map);
 }
 
-module.exports.transformSIUrl = transformSIUrl;
-module.exports.transformGame = transformGame;
 module.exports.getInProgressAndCompletedGames = getInProgressAndCompletedGames;
-module.exports.transformGameV2 = transformGameV2;
-module.exports.getStatusV2 = getStatusV2;
+module.exports.transformGame = transformGame;
+module.exports.getStatus = getStatus;
