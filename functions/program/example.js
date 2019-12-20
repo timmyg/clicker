45f747c9b3e537477440e341447e015d4b3f67f1
tim <timmyg13@gmail.com>
2019-12-20 10:43:19 -0500

    health test

--- functions/program/example.js
+++ functions/program/example.js
@@ -1,16 +1,4 @@
 console.log(4);
 (async () => {
-  console.log('1');
-  require('dotenv').config({ path: '../.env' });
-  const Airtable = require('airtable');
-  const programmingId = 'SH033462440000';
-  const airtableBase = new Airtable({ apiKey: process.env.airtableKey }).base(process.env.airtableBase);
-  const airtablePrograms = 'Programs';
-  console.log('call');
-  const airtableGames = await airtableBase(airtablePrograms)
-    .select({
-      filterByFormula: `{programmingId}='${programmingId}'`,
-    })
-    .all();
-  console.log({ airtableGames });
+  const axios = require('axios');
 })();
