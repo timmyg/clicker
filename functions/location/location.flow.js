667e69358a9920dd5d7b39f48ae9cb42e1e8a41b
Tim <tim.giblin.contractor@8451.com>
2020-01-02 15:51:36 -0500

    fix program

--- functions/location/location.flow.js
+++ functions/location/location.flow.js
@@ -693,20 +693,22 @@ module.exports.updateBoxesProgram = RavenLambdaWrapper.handler(Raven, async even
       const { channel, id: boxId } = box;
       // const boxIndex: number = location.boxes.findIndex(b => b.id === boxId);
       console.log('get program', { channel, region });
-      const { data: program } = await new Invoke()
+      const result = await new Invoke()
         .service('program')
         .name('get')
         .queryParams({ channel, region })
         .go();
-      console.log({ program });
-
-      await new Invoke()
-        .service('location')
-        .name('updateBoxInfo')
-        .body({ program })
-        .pathParams({ id: locationId, boxId })
-        .async()
-        .go();
+      if (result.data) {
+        await new Invoke()
+          .service('location')
+          .name('updateBoxInfo')
+          .body({ program: result.data })
+          .pathParams({ id: locationId, boxId })
+          .async()
+          .go();
+      } else {
+        console.log('no program found:', { channel, region });
+      }
       i++;
     }
   }
