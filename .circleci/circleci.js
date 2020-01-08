f06b97c6c4f3e055dee2fd431e9894c167c0b0ca
Tim <tim.giblin.contractor@8451.com>
2020-01-08 13:33:55 -0500

    new circle ci

---
+++ .circleci/circleci.js
@@ -0,0 +1,35 @@
+const fetch = require("node-fetch");
+const { exec } = require("child_process");
+
+const modules = ["core", "components"];
+
+exec("npx lerna changed --all", (error, stdout) => {
+  modules.forEach(name => {
+    console.log({ name });
+    if (!stdout.includes(name)) return;
+
+    const params = {
+      parameters: {
+        [name]: true,
+        trigger: false
+      },
+      branch: process.env.CIRCLE_BRANCH
+    };
+
+    fetch(
+      `https://circleci.com/api/v2/project/github/${
+        process.env.CIRCLE_PROJECT_USERNAME
+      }/${process.env.CIRCLE_PROJECT_REPONAME}/pipeline`,
+      {
+        body: JSON.stringify(params),
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            process.env.CIRCLE_TOKEN
+          ).toString("base64")}`,
+          "Content-Type": "application/json"
+        },
+        method: "POST"
+      }
+    ).then(console.log);
+  });
+});
