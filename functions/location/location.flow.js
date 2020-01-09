0e045d9edb7525eedbb9db2b42986718297dcd0b
Tim <tim.giblin.contractor@8451.com>
2020-01-09 09:43:42 -0500

    upgrade circle

--- functions/location/location.flow.js
+++ functions/location/location.flow.js
@@ -662,6 +662,7 @@ module.exports.controlCenterLocationsByRegion = RavenLambdaWrapper.handler(Raven
   return respond(200, locations);
 });
 
+
 module.exports.health = async (event: any) => {
   return respond(200, 'ok');
 };
