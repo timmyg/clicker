9a3abcedab7d460d97982d5e8ade9f56f8a25cc5
Tim <tim.giblin.contractor@8451.com>
2020-01-09 15:42:01 -0500

    touch

--- functions/analytics/analytics.flow.js
+++ functions/analytics/analytics.flow.js
@@ -13,7 +13,7 @@ module.exports.track = RavenLambdaWrapper.handler(Raven, async event => {
     auth: {
       username: process.env.segmentWriteKey,
     },
-  };
+  }; 
 
   console.log('https://api.segment.io/v1/track', body, options);
   console.time('track event');
