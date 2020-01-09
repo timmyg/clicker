205517141bed9a1aaa01fce762c70cdabd48af46
Tim <tim.giblin.contractor@8451.com>
2020-01-09 11:42:03 -0500

    upgrade circle

--- .circleci/circle_trigger.js
+++ .circleci/circle_trigger.js
@@ -1,22 +1,23 @@
-const fetch = require('node-fetch');
-const { exec } = require('child_process');
+const fetch = require("node-fetch");
+const { exec } = require("child_process");
 
-const circleUrl = `https://circleci.com/api/v1.1/project/github/${process.env.CIRCLE_PROJECT_USERNAME}/${process.env.CIRCLE_PROJECT_REPONAME}/tree/${process.env.CIRCLE_BRANCH}?filter=completed&limit=1`
-console.log({circleUrl});
-fetch(
-    circleUrl,
-    {
-        // body: JSON.stringify(params),
-        headers: {
-            Authorization: `Basic ${Buffer.from(
-                process.env.CIRCLE_TOKEN
-            ).toString('base64')}`,
-            'Content-Type': 'application/json',
-        },
-        method: 'GET',
-    }
-).then(console.log);
-});
+const circleUrl = `https://circleci.com/api/v1.1/project/github/${
+  process.env.CIRCLE_PROJECT_USERNAME
+}/${process.env.CIRCLE_PROJECT_REPONAME}/tree/${
+  process.env.CIRCLE_BRANCH
+}?filter=completed&limit=1`;
+console.log({ circleUrl });
+fetch(circleUrl, {
+  // body: JSON.stringify(params),
+  headers: {
+    Authorization: `Basic ${Buffer.from(process.env.CIRCLE_TOKEN).toString(
+      "base64"
+    )}`,
+    "Content-Type": "application/json"
+  },
+  method: "GET"
+}).then(console.log);
+// });
 
 // exec('yarn check:changes', (error, stdout) => {
 //     modules.forEach(name => {
--- functions/circleci.js
+++ functions/circleci.js
@@ -1,34 +1,32 @@
-const fetch = require("node-fetch");
-const { exec } = require("child_process");
+const fetch = require('node-fetch');
+const { exec } = require('child_process');
 
-const modules = ["core", "components"];
+const modules = ['core', 'components'];
 
-exec("yarn check:changes", (error, stdout) => {
+exec('yarn check:changes', (error, stdout) => {
   modules.forEach(name => {
     if (!stdout.includes(name)) return;
 
     const params = {
       parameters: {
         [name]: true,
-        trigger: false
+        trigger: false,
       },
-      branch: process.env.CIRCLE_BRANCH
+      branch: process.env.CIRCLE_BRANCH,
     };
 
     fetch(
-      `https://circleci.com/api/v2/project/github/${
-        process.env.CIRCLE_PROJECT_USERNAME
-      }/${process.env.CIRCLE_PROJECT_REPONAME}/pipeline`,
+      `https://circleci.com/api/v2/project/github/${process.env.CIRCLE_PROJECT_USERNAME}/${
+        process.env.CIRCLE_PROJECT_REPONAME
+      }/pipeline`,
       {
         body: JSON.stringify(params),
         headers: {
-          Authorization: `Basic ${Buffer.from(
-            process.env.CIRCLE_TOKEN
-          ).toString("base64")}`,
-          "Content-Type": "application/json"
+          Authorization: `Basic ${Buffer.from(process.env.CIRCLE_TOKEN).toString('base64')}`,
+          'Content-Type': 'application/json',
         },
-        method: "POST"
-      }
+        method: 'POST',
+      },
     ).then(console.log);
   });
 });
