d2caf9251b2d60750500d0b03f008a2bc67abf41
Tim <tim.giblin.contractor@8451.com>
2020-01-09 13:16:34 -0500

    node-fetch

--- .circleci/circle_trigger.js
+++ .circleci/circle_trigger.js
@@ -39,32 +39,31 @@ const fs = require("fs");
     activeProjects.includes(p)
   );
   console.log({ changedActiveProjects });
-  
-  // exec('yarn check:changes', (error, stdout) => {
-  //     modules.forEach(name => {
-  //         if (!stdout.includes(name)) return;
 
-  //         const params = {
-  //             parameters: {
-  //                 [name]: true,
-  //                 trigger: false,
-  //             },
-  //             branch: process.env.CIRCLE_BRANCH,
-  //         };
+  for (const changedActiveProject of changedActiveProjects) {
+    const params = {
+      parameters: {
+        [changedActiveProject]: true,
+        trigger: false
+      },
+      branch: process.env.CIRCLE_BRANCH
+    };
 
-  //         fetch(
-  //             `https://circleci.com/api/v2/project/github/${process.env.CIRCLE_PROJECT_USERNAME}/${process.env.CIRCLE_PROJECT_REPONAME}/pipeline`,
-  //             {
-  //                 body: JSON.stringify(params),
-  //                 headers: {
-  //                     Authorization: `Basic ${Buffer.from(
-  //                         process.env.CIRCLE_TOKEN
-  //                     ).toString('base64')}`,
-  //                     'Content-Type': 'application/json',
-  //                 },
-  //                 method: 'POST',
-  //             }
-  //         ).then(console.log);
-  //     });
-  // });
+    console.log({ params });
+    await fetch(
+      `https://circleci.com/api/v2/project/github/${
+        process.env.CIRCLE_PROJECT_USERNAME
+      }/${process.env.CIRCLE_PROJECT_REPONAME}/pipeline`,
+      {
+        body: JSON.stringify(params),
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            process.env.CIRCLE_TOKEN
+          ).toString("base64")}`,
+          "Content-Type": "application/json"
+        },
+        method: "POST"
+      }
+    );
+  }
 })();
