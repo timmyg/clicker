c3e86767e7adc8184e50f2afe5548794d2e411ed
Tim <tim.giblin.contractor@8451.com>
2020-01-09 15:25:38 -0500

    touch

--- functions/analytics/analytics.flow.js
+++ functions/analytics/analytics.flow.js
@@ -26,4 +26,3 @@ module.exports.track = RavenLambdaWrapper.handler(Raven, async event => {
 module.exports.health = RavenLambdaWrapper.handler(Raven, async event => {
   return respond(200, `hello`);
 });
-
