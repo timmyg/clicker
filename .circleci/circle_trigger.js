88f03295697a9e81271b7e4e7b3284b0ee828da4
Tim <tim.giblin.contractor@8451.com>
2020-01-09 12:19:40 -0500

    node-fetch

--- .circleci/circle_trigger.js
+++ .circleci/circle_trigger.js
@@ -1,51 +1,55 @@
 const fetch = require("node-fetch");
 const { exec } = require("child_process");
 
-const circleUrl = `https://circleci.com/api/v1.1/project/github/${
-  process.env.CIRCLE_PROJECT_USERNAME
-}/${process.env.CIRCLE_PROJECT_REPONAME}/tree/${
-  process.env.CIRCLE_BRANCH
-}?filter=completed&limit=1`;
-console.log({ circleUrl });
-fetch(circleUrl, {
-  // body: JSON.stringify(params),
-  headers: {
-    Authorization: `Basic ${Buffer.from(process.env.CIRCLE_TOKEN).toString(
-      "base64"
-    )}`,
-    "Content-Type": "application/json"
-  },
-  method: "GET"
-})
-  .then(response => response.json())
-  .then(data => {
-    data;
-  });
+async function go() {
+  const circleUrl = `https://circleci.com/api/v1.1/project/github/${
+    process.env.CIRCLE_PROJECT_USERNAME
+  }/${process.env.CIRCLE_PROJECT_REPONAME}/tree/${
+    process.env.CIRCLE_BRANCH
+  }?filter=completed&limit=1`;
+  console.log({ circleUrl });
+  fetch(circleUrl, {
+    // body: JSON.stringify(params),
+    headers: {
+      Authorization: `Basic ${Buffer.from(process.env.CIRCLE_TOKEN).toString(
+        "base64"
+      )}`,
+      "Content-Type": "application/json"
+    },
+    method: "GET"
+  })
+    .then(response => response.json())
+    .then(data => {
+      data;
+    });
 
-// exec('yarn check:changes', (error, stdout) => {
-//     modules.forEach(name => {
-//         if (!stdout.includes(name)) return;
+  // exec('yarn check:changes', (error, stdout) => {
+  //     modules.forEach(name => {
+  //         if (!stdout.includes(name)) return;
 
-//         const params = {
-//             parameters: {
-//                 [name]: true,
-//                 trigger: false,
-//             },
-//             branch: process.env.CIRCLE_BRANCH,
-//         };
+  //         const params = {
+  //             parameters: {
+  //                 [name]: true,
+  //                 trigger: false,
+  //             },
+  //             branch: process.env.CIRCLE_BRANCH,
+  //         };
 
-//         fetch(
-//             `https://circleci.com/api/v2/project/github/${process.env.CIRCLE_PROJECT_USERNAME}/${process.env.CIRCLE_PROJECT_REPONAME}/pipeline`,
-//             {
-//                 body: JSON.stringify(params),
-//                 headers: {
-//                     Authorization: `Basic ${Buffer.from(
-//                         process.env.CIRCLE_TOKEN
-//                     ).toString('base64')}`,
-//                     'Content-Type': 'application/json',
-//                 },
-//                 method: 'POST',
-//             }
-//         ).then(console.log);
-//     });
-// });
+  //         fetch(
+  //             `https://circleci.com/api/v2/project/github/${process.env.CIRCLE_PROJECT_USERNAME}/${process.env.CIRCLE_PROJECT_REPONAME}/pipeline`,
+  //             {
+  //                 body: JSON.stringify(params),
+  //                 headers: {
+  //                     Authorization: `Basic ${Buffer.from(
+  //                         process.env.CIRCLE_TOKEN
+  //                     ).toString('base64')}`,
+  //                     'Content-Type': 'application/json',
+  //                 },
+  //                 method: 'POST',
+  //             }
+  //         ).then(console.log);
+  //     });
+  // });
+}
+
+go();
