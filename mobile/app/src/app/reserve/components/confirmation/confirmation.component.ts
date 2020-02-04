1fe7d28c6852f90ff03fc18056fc785ad71030e6
Tim <tim.giblin.contractor@8451.com>
2020-02-04 13:13:33 -0500

    updates

--- mobile/app/src/app/reserve/components/confirmation/confirmation.component.ts
+++ mobile/app/src/app/reserve/components/confirmation/confirmation.component.ts
@@ -1,29 +1,34 @@
-import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
-import { Reservation } from '../../../state/reservation/reservation.model';
-import { ReserveService } from '../../reserve.service';
-import { Observable, Subscription } from 'rxjs';
-import { Store, select } from '@ngrx/store';
-import { getReservation } from 'src/app/state/reservation';
-import * as fromStore from '../../../state/app.reducer';
-import * as fromReservation from '../../../state/reservation/reservation.actions';
-import { Router, ActivatedRoute } from '@angular/router';
-import * as moment from 'moment';
-import { ToastController } from '@ionic/angular';
-import { first, filter } from 'rxjs/operators';
-import { isLoggedIn, getUserTokenCount } from 'src/app/state/user';
-import { Actions, ofType } from '@ngrx/effects';
-import { SegmentService } from 'ngx-segment-analytics';
-import { Globals } from 'src/app/globals';
-import { Timeframe } from 'src/app/state/app/timeframe.model';
-import { getTimeframes } from 'src/app/state/app';
-import * as fromApp from 'src/app/state/app/app.actions';
-import { getLoading as getAppLoading } from 'src/app/state/app';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ChangeDetectionStrategy
+} from "@angular/core";
+import { Reservation } from "../../../state/reservation/reservation.model";
+import { ReserveService } from "../../reserve.service";
+import { Observable, Subscription } from "rxjs";
+import { Store, select } from "@ngrx/store";
+import { getReservation } from "src/app/state/reservation";
+import * as fromStore from "../../../state/app.reducer";
+import * as fromReservation from "../../../state/reservation/reservation.actions";
+import { Router, ActivatedRoute } from "@angular/router";
+import * as moment from "moment";
+import { ToastController } from "@ionic/angular";
+import { first, filter } from "rxjs/operators";
+import { isLoggedIn, getUserTokenCount } from "src/app/state/user";
+import { Actions, ofType } from "@ngrx/effects";
+import { SegmentService } from "ngx-segment-analytics";
+import { Globals } from "src/app/globals";
+import { Timeframe } from "src/app/state/app/timeframe.model";
+import { getTimeframes } from "src/app/state/app";
+import * as fromApp from "src/app/state/app/app.actions";
+import { getLoading as getAppLoading } from "src/app/state/app";
 
 @Component({
-  selector: 'app-confirmation',
-  templateUrl: './confirmation.component.html',
-  styleUrls: ['./confirmation.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush, // ExpressionChangedAfterItHasBeenCheckedError when opening wallet if not here
+  selector: "app-confirmation",
+  templateUrl: "./confirmation.component.html",
+  styleUrls: ["./confirmation.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush // ExpressionChangedAfterItHasBeenCheckedError when opening wallet if not here
 })
 export class ConfirmationComponent implements OnDestroy, OnInit {
   timeframes$: Observable<Timeframe[]>;
@@ -33,7 +38,7 @@ export class ConfirmationComponent implements OnDestroy, OnInit {
   tokenCount: number;
   isLoggedIn$: Observable<boolean>;
   reservation: Partial<Reservation>;
-  title = 'Confirmation';
+  title = "Confirmation";
   isLoggedIn: boolean;
   saving: boolean;
   isEditMode: boolean;
@@ -42,11 +47,12 @@ export class ConfirmationComponent implements OnDestroy, OnInit {
   outOfRange: boolean;
   isAppLoading$: Observable<boolean>;
   isAppLoading: boolean;
+  isReservationLengthEdit: boolean;
   isInitializing = true;
   sub: Subscription;
   timeframe0: Timeframe = {
     minutes: 0,
-    tokens: 0,
+    tokens: 0
   };
   overideDistanceClicks = 0;
   overrideDistanceDisable = false;
@@ -59,7 +65,7 @@ export class ConfirmationComponent implements OnDestroy, OnInit {
     private actions$: Actions,
     private segment: SegmentService,
     private globals: Globals,
-    private route: ActivatedRoute,
+    private route: ActivatedRoute
   ) {
     this.timeframes$ = this.store.select(getTimeframes);
     this.reservation$ = this.store.select(getReservation);
@@ -76,49 +82,53 @@ export class ConfirmationComponent implements OnDestroy, OnInit {
   ngOnDestroy() {
     // clear timeframes because it messes up the radio buttons when reloading
     this.store.dispatch(new fromApp.ClearTimeframes());
-    if (this.sub) this.sub.unsubscribe();
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
   ngOnInit() {
     this.reservation$
       .pipe(
         filter(r => r !== null),
-        first(),
+        first()
       )
       .subscribe(reservation => {
         if (reservation.location.distance > this.rangeDistanceMiles) {
           this.outOfRange = true;
         }
-        this.store.dispatch(new fromApp.LoadTimeframes(reservation.location.id));
+        this.store.dispatch(
+          new fromApp.LoadTimeframes(reservation.location.id)
+        );
         this.reservation = reservation;
         // initialize reservation
-        if (this.reservation.location.free) {
-          this.reservation.cost = 0;
-          this.reservation.minutes = 0;
-          this.isInitializing = false;
-        } else {
-          this.timeframes$
-            .pipe(
-              filter(t => t !== null),
-              first(),
-            )
-            .subscribe(timeframes => {
-              if (this.reservation.minutes !== 0) {
-                const timeframe = timeframes[0];
-                this.reservation.cost = timeframe.tokens;
-                this.reservation.minutes = timeframe.minutes;
-              }
-              this.isInitializing = false;
-            });
-        }
+        // if (this.reservation.location.free) {
+        //   this.reservation.cost = 0;
+        //   this.reservation.minutes = 0;
+        //   this.isInitializing = false;
+        // } else {
+        this.timeframes$
+          .pipe(
+            filter(t => t !== null),
+            first()
+          )
+          .subscribe(timeframes => {
+            if (this.reservation.minutes !== 0) {
+              const timeframe = timeframes[0];
+              this.reservation.cost = timeframe.tokens;
+              this.reservation.minutes = timeframe.minutes;
+            }
+            this.isInitializing = false;
+          });
+        // }
         this.route.queryParams.subscribe(params => {
           // this.reservation.minutes = this.reservation.location.minutes;
           if (params && params.edit) {
-            if (params.edit === 'channel') {
+            if (params.edit === "channel") {
               this.reservation.minutes = 0;
+            } else if (params.edit === "time") {
+              this.isReservationLengthEdit = true;
             }
-            // else if (params.edit === 'time') {
-            // }
           }
         });
         if (reservation.id) {
@@ -135,7 +145,7 @@ export class ConfirmationComponent implements OnDestroy, OnInit {
 
   getEndTime() {
     return moment(this.reservation.end)
-      .add(this.reservation.minutes.valueOf(), 'minutes')
+      .add(this.reservation.minutes.valueOf(), "minutes")
       .toDate();
   }
 
@@ -144,7 +154,7 @@ export class ConfirmationComponent implements OnDestroy, OnInit {
   }
 
   onConfirm() {
-    console.log('onconfirm');
+    console.log("onconfirm");
     const { reservation: r } = this;
     this.saving = true;
     this.isEditMode
@@ -152,10 +162,15 @@ export class ConfirmationComponent implements OnDestroy, OnInit {
       : this.store.dispatch(new fromReservation.Create(r));
     const reservation = r;
     this.actions$
-      .pipe(ofType(fromReservation.CREATE_RESERVATION_SUCCESS, fromReservation.UPDATE_RESERVATION_SUCCESS))
+      .pipe(
+        ofType(
+          fromReservation.CREATE_RESERVATION_SUCCESS,
+          fromReservation.UPDATE_RESERVATION_SUCCESS
+        )
+      )
       .pipe(first())
       .subscribe(() => {
-        console.log('success');
+        console.log("success");
         if (this.isEditMode) {
           this.segment.track(this.globals.events.reservation.updated, {
             minutes: r.minutes,
@@ -164,7 +179,7 @@ export class ConfirmationComponent implements OnDestroy, OnInit {
             channelName: r.program.channelTitle,
             channelNumber: r.program.channel,
             programName: r.program.title,
-            programDescription: r.program.description,
+            programDescription: r.program.description
           });
         } else {
           this.segment.track(this.globals.events.reservation.created, {
@@ -174,15 +189,23 @@ export class ConfirmationComponent implements OnDestroy, OnInit {
             channelName: r.program.channelTitle,
             channelNumber: r.program.channel,
             programName: r.program.title,
-            programDescription: r.program.description,
+            programDescription: r.program.description
           });
         }
         this.store.dispatch(new fromReservation.Start());
-        this.router.navigate(['/tabs/profile']);
-        this.showTunedToast(reservation.box.label, reservation.program.channelTitle);
+        this.router.navigate(["/tabs/profile"]);
+        this.showTunedToast(
+          reservation.box.label,
+          reservation.program.channelTitle
+        );
       });
     this.actions$
-      .pipe(ofType(fromReservation.CREATE_RESERVATION_FAIL, fromReservation.UPDATE_RESERVATION_FAIL))
+      .pipe(
+        ofType(
+          fromReservation.CREATE_RESERVATION_FAIL,
+          fromReservation.UPDATE_RESERVATION_FAIL
+        )
+      )
       .pipe(first())
       .subscribe(async () => {
         this.showErrorToast();
@@ -195,7 +218,7 @@ export class ConfirmationComponent implements OnDestroy, OnInit {
     const toast = await this.toastController.create({
       message: `📺 ${label} successfully changed to ${channelName}. ⚡`,
       duration: 2000,
-      cssClass: 'ion-text-center',
+      cssClass: "ion-text-center"
     });
     toast.present();
   }
@@ -204,8 +227,8 @@ export class ConfirmationComponent implements OnDestroy, OnInit {
     const toast = await this.toastController.create({
       message: `Something went wrong, please try again.`,
       duration: 2000,
-      cssClass: 'ion-text-center',
-      color: 'danger',
+      cssClass: "ion-text-center",
+      color: "danger"
     });
     toast.present();
   }
