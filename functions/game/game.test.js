e1a2a92ddd653982c0813b7565378cc484ecdf19
tim <timmyg13@gmail.com>
2020-02-07 18:10:29 -0500

    fixes

--- functions/game/game.test.js
+++ functions/game/game.test.js
@@ -29,8 +29,8 @@ describe('transformGame', () => {
     expect(result.home.name.display).toBe('Baylor');
     expect(result.home.logo).toBe('https://static.sprtactn.co/teamlogos/ncaaf/100/bay.png');
     expect(result.home.rank).toBe(12);
-    expect(result.home.book.moneyline).toBe(-850);
-    expect(result.home.book.spread).toBe(-18);
+    expect(result.home.book.moneyline).toBe('-850');
+    expect(result.home.book.spread).toBe('-18');
     expect(result.away.score).toBe(14);
     expect(result.away.name.full).toBe('West Virginia Mountaineers');
     expect(result.away.name.short).toBe('Mountaineers');
@@ -38,8 +38,8 @@ describe('transformGame', () => {
     expect(result.away.name.display).toBe('West Virginia');
     expect(result.away.logo).toBe('https://static.sprtactn.co/teamlogos/ncaaf/100/wvu.png');
     expect(result.away.rank).toBeFalsy();
-    expect(result.away.book.moneyline).toBe(575);
-    expect(result.away.book.spread).toBe(18);
+    expect(result.away.book.moneyline).toBe('+575');
+    expect(result.away.book.spread).toBe('+18');
   });
 });
 describe('get status', () => {
