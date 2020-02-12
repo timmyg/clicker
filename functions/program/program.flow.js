efa20cb640264bc883a2e36f805342a0ce67144e
tim <timmyg13@gmail.com>
2020-02-11 22:51:18 -0500

    isOver

--- functions/program/program.flow.js
+++ functions/program/program.flow.js
@@ -540,8 +540,11 @@ async function getAirtableProgramsInWindow(hoursAgo = 4, hoursFromNow = 4) {
     .add(hoursFromNow, 'h')
     .toISOString();
 
-  let filterByFormula: string[] = [`{start} > '${fourHoursAgo}'`, `{start} < '${fourHoursFromNow}'`];
+  let filterByFormula: string[] = [];
+  filterByFormula.push(`{start} > '${fourHoursAgo}'`);
+  filterByFormula.push(`{start} < '${fourHoursFromNow}'`);
   filterByFormula.push(`{rating} != BLANK()`);
+  filterByFormula.push(`{isOver} != 'Y'`);
   const updatedAirtablePrograms = await base(airtableProgramsName)
     .select({
       filterByFormula: `AND(${filterByFormula.join(',')})`,
