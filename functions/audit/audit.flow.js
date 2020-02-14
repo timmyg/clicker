ed8dc565b6748d1a2005c04d60a8e97391678a5b
Tim <tim.giblin.contractor@8451.com>
2020-02-14 11:29:59 -0500

    updates

--- functions/audit/audit.flow.js
+++ functions/audit/audit.flow.js
@@ -1,16 +1,20 @@
-// // @flow
-// const { getBody, respond, Invoke, Raven, RavenLambdaWrapper } = require('serverless-helpers');
+// @flow
+const { getBody, respond, Invoke, Raven, RavenLambdaWrapper } = require('serverless-helpers');
 
-// // declare class process {
-// //   static env: {
-// //     airtableKey: string,
-// //     airtableBase: string,
-// //     circleToken: string,
-// //     stage: string,
-// //   };
-// // }
+// declare class process {
+//   static env: {
+//     airtableKey: string,
+//     airtableBase: string,
+//     circleToken: string,
+//     stage: string,
+//   };
+// }
 
-// module.exports.health = RavenLambdaWrapper.handler(Raven, async event => {
-//   const body = getBody(event);
-//   return respond(200), 200;
-// });
+module.exports.health = RavenLambdaWrapper.handler(Raven, async event => {
+  return respond(200, `hello`);
+});
+
+module.exports.create = RavenLambdaWrapper.handler(Raven, async event => {
+  const body = getBody(event);
+  return respond(200), 200;
+});
