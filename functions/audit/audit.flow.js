d45c5a88c32d3c1aab73a6690df4e7b336dc14ff
Tim <tim.giblin.contractor@8451.com>
2020-02-14 12:21:24 -0500

    audit

--- functions/audit/audit.flow.js
+++ functions/audit/audit.flow.js
@@ -1,6 +1,7 @@
 // @flow
 const { getBody, respond, Invoke, Raven, RavenLambdaWrapper } = require('serverless-helpers');
-
+const dynamo = require('dynamodb');
+const Joi = require('@hapi/joi');
 // declare class process {
 //   static env: {
 //     airtableKey: string,
@@ -10,11 +11,25 @@ const { getBody, respond, Invoke, Raven, RavenLambdaWrapper } = require('serverl
 //   };
 // }
 
+const Audit = dynamo.define(process.env.tableReservation, {
+  hashKey: 'date',
+  rangeKey: 'entityId',
+
+  // add the timestamp attributes (updatedAt, createdAt)
+  timestamps: true,
+
+  schema: {
+    type: Joi.string(),
+  },
+});
+
 module.exports.health = RavenLambdaWrapper.handler(Raven, async event => {
   return respond(200, `hello`);
 });
 
 module.exports.create = RavenLambdaWrapper.handler(Raven, async event => {
-  const body = getBody(event);
-  return respond(200), 200;
+  const { date, entityId, type } = getBody(event);
+  var audit = new Audit({ date, entityId, type });
+  const result = await audit.save();
+  return respond(200, result);
 });
