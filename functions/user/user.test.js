0ed2b090c4a819aa190bb837c13abda231a59b4a
Tim <tim.giblin.contractor@8451.com>
2020-02-17 17:20:01 -0500

    fix test

--- functions/user/user.test.js
+++ functions/user/user.test.js
@@ -8,19 +8,19 @@ test('smoke test', () => {
 
 describe('cost per tokens', () => {
   test('5 tokens, $10', () => {
-    const response = getTokenValue({ lifetime: { tokens: 5, spent: 10 } });
+    const response = getTokenValue({ lifetimeTokens: 5, lifetimeSpent: 10 });
     expect(response).toBe(2);
   });
   test('6 tokens, $15', () => {
-    const response = getTokenValue({ lifetime: { tokens: 6, spent: 15 } });
+    const response = getTokenValue({ lifetimeTokens: 6, lifetimeSpent: 15 });
     expect(response).toBe(2.5);
   });
   test('7 tokens, $20', () => {
-    const response = getTokenValue({ lifetime: { tokens: 7, spent: 20 } });
+    const response = getTokenValue({ lifetimeTokens: 7, lifetimeSpent: 20 });
     expect(response).toBe(2.86);
   });
   test('3 tokens, $10', () => {
-    const response = getTokenValue({ lifetime: { tokens: 3, spent: 10 } });
+    const response = getTokenValue({ lifetimeTokens: 3, lifetimeSpent: 10 });
     expect(response).toBe(3.34);
   });
 });
