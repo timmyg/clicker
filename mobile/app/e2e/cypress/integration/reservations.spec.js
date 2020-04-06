ff8404fa722e609cd8297da49d4c745c600d40f5
Tim <tim.giblin.contractor@8451.com>
2020-04-06 14:54:41 -0400

    fixes

--- mobile/app/e2e/cypress/integration/reservations.spec.js
+++ mobile/app/e2e/cypress/integration/reservations.spec.js
@@ -1,5 +1,5 @@
 // TODO remove these when the app isnt so slow
-const timeout = 8000
+const timeout = 8000;
 Cypress.config("pageLoadTimeout", timeout);
 Cypress.config("defaultCommandTimeout", timeout);
 
@@ -21,36 +21,38 @@ context("Reservations", () => {
     cy.request("DELETE", `https://api-develop.tryclicker.com/locations/${id}`);
   });
 
-  beforeEach("login + set geolocation", done => {
+  beforeEach("login + set geolocation", (done) => {
     cy.initialSetup(() => {
       return done();
     });
   });
 
-  beforeEach("go to root page", done => {
+  beforeEach("go to root page", (done) => {
     cy.landingPage(() => {
       return done();
     });
   });
 
   it("should create a reservation", () => {
-    cy.get("app-coins .count")
-      .then(($count) => {
-        expect($count).to.have.text('1')
-      })
+    cy.get("app-coins .count").then(($count) => {
+      cy.log("count", $count);
+      expect($count).to.have.text("1");
+    });
     cy.get("app-location:nth-of-type(1) ion-card-content h1")
-      .contains('Test Wicked Wolf').click();
-    cy.get("ion-list[data-atm='programs']:not(.content-loading) app-program:nth-of-type(2) ion-card-content .title")
-      .click({ force: true });
+      .contains("Test Wicked Wolf")
+      .click();
+    cy.get(
+      "ion-list[data-atm='programs']:not(.content-loading) app-program:nth-of-type(2) ion-card-content .title"
+    ).click({ force: true });
     cy.get("app-tvs ion-button")
       .contains("2")
       .click();
     cy.get("ion-button#confirm:not([disabled])").click();
-    cy.get("app-reservation").should($reservations => {
+    cy.get("app-reservation").should(($reservations) => {
       expect($reservations).to.have.length(1);
     });
     cy.get("app-coins")
       .find(".count")
-      .contains("0")
+      .contains("0");
   });
 });
