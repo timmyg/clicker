9e4233ae9b2c15c723ce85b83cca02b8b7fe3474
tim <timmyg13@gmail.com>
2020-04-08 21:27:05 -0400

    build

--- mobile/app/e2e/cypress/support/commands.js
+++ mobile/app/e2e/cypress/support/commands.js
@@ -11,7 +11,7 @@
 //
 
 Cypress.Commands.add("landingPage", (callback) => {
-  const baseUrl = "http://localhost:4100";
+  const baseUrl = "http://localhost:8080";
   cy.fixture("geolocation.json").as("fakeLocation");
   cy.get("@fakeLocation").then((fakeLocation) => {
     cy.visit(baseUrl, {
