f3c123cfd9f01b0cb32264596c21d28d6f48fab7
tim <timmyg13@gmail.com>
2020-04-10 14:12:16 -0400

    fix some tsconfig stuff

--- mobile/app/src/app/pipes/truncate.pipe.ts
+++ mobile/app/src/app/pipes/truncate.pipe.ts
@@ -6,8 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class TruncatePipe implements PipeTransform {
 
-transform(value: string, args: string[]): string {
-    const limit = args.length > 0 ? parseInt(args[0], 10) : 20;
+transform(value: string, args: any[]): string {
+    const limit = args.length > 0 ? args[0] : 20;
     const trail = args.length > 1 ? args[1] : '...';
     return value.length > limit ? value.substring(0, limit) + trail : value;
    }
