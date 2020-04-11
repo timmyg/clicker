2ea37003d3be606100238a660ba05b0d6a8ba9c4
tim <timmyg13@gmail.com>
2020-04-11 13:57:01 -0400

    env configuration

--- mobile/app/e2e/cypress/integration/reservations.spec.js
+++ mobile/app/e2e/cypress/integration/reservations.spec.js
@@ -3,53 +3,39 @@ const timeout = 12000;
 Cypress.config("pageLoadTimeout", timeout);
 Cypress.config("defaultCommandTimeout", timeout);
 
-// const environment = require('../../../src/environments/environment');
+const apiBaseUrl =
+  Cypress.env("apiBaseUrl") || "https://api-develop.tryclicker.com";
 
-// describe('creating reservations', () => {
 context("Reservations", () => {
-  beforeEach(() => {
-    // run these tests as if in a desktop
-    // browser with a 720p monitor
-    cy.viewport(1280, 720)
-  })
   before("create test location", () => {
+    cy.log("apiBaseUrl", apiBaseUrl);
     const location = require("../fixtures/requests/location.json");
-    cy.request(
-      "POST",
-      "https://api-develop.tryclicker.com/locations",
-      location
-    );
+    cy.request("POST", `${apiBaseUrl}/locations`, location);
   });
 
   after("delete test location", () => {
     const { id } = require("../fixtures/requests/location.json");
-    cy.request("DELETE", `https://api-develop.tryclicker.com/locations/${id}`);
+    cy.request("DELETE", `${apiBaseUrl}/locations/${id}`);
   });
 
-  beforeEach("login + set geolocation", (done) => {
+  beforeEach("login + set geolocation", done => {
     cy.initialSetup(() => {
       return done();
     });
   });
 
-  beforeEach("go to root page", (done) => {
+  beforeEach("go to root page", done => {
     cy.landingPage(() => {
       return done();
     });
   });
 
   it("should create a reservation", () => {
-    // cy.get("app-coins .coins")
-    //   .then(($count) => {
-    //     cy.log("count", $count);
-    //     expect($count).to.have.text("1");
-    //   });
     cy.get("app-coins")
       .find(".count")
       .contains("1");
     cy.get("app-location:nth-of-type(1) ion-card-content h1")
       .contains("Test Wicked Wolf")
-      // .contains("blha blah bad")
       .click({ force: true });
     cy.get(
       "ion-list[data-atm='programs']:not(.content-loading) app-program:nth-of-type(2) ion-card-content .title"
@@ -58,7 +44,7 @@ context("Reservations", () => {
       .contains("2")
       .click({ force: true });
     cy.get("ion-button#confirm:not([disabled])").click({ force: true });
-    cy.get("app-reservation").should(($reservations) => {
+    cy.get("app-reservation").should($reservations => {
       expect($reservations).to.have.length(1);
     });
     cy.get("app-coins")
