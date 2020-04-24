f1aced395fe478c563f8cf88e5cae6dfdd225359
tim <timmyg13@gmail.com>
2020-04-23 22:08:13 -0400

    label

--- functions/location/location.flow.js
+++ functions/location/location.flow.js
@@ -1117,7 +1117,7 @@ module.exports.getLocationDetailsPage = RavenLambdaWrapper.handler(Raven, async 
       <ul> \
       {{#boxes}} \
         <li> \
-          <b>Box {{zone || label}}</b>: {{live.program.channelTitle}} <em>{{live.program.title}}</em> \
+          <b>Box {{#zone}}{{zone}}{{/zone}}{{#label}}{{label}}{{/label}}</b>: {{live.program.channelTitle}} <em>{{live.program.title}}</em> \
         </li> \
       {{/boxes}} \
       </ul> \ 
