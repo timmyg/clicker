f23a9ad7b36b74e769aba0d48a1955e7596d40b1
tim <timmyg13@gmail.com>
2020-04-27 13:49:24 -0400

    blog

--- web/app/components/landing/BlogPreview.vue
+++ web/app/components/landing/BlogPreview.vue
@@ -4,12 +4,44 @@
       <div class="pricing-inner section-inner">
         <div class="center-content mb-16">
           <div class="container-xs">
-            <h2 class="m-0" v-scroll-reveal>From the Blog</h2>
+            <!-- <h2 class="m-0" v-scroll-reveal>From the Blog</h2> -->
+            <h2 class="brand-font text-3xl pt-4">From the Blog</h2>
           </div>
         </div>
-        <div class="center-content container mb-64">
-          <!-- <div class="container mb-64" v-if="latestPost">{{latestPost.fields.title}}</div>
-          <div class="container mb-64" v-if="randomPost">{{randomPost.fields.title}}</div>-->
+        <div class>
+          <div
+            class="w-1/2 lg:w-1/4 inline-block text-center"
+            v-for="(post, index) in recentPosts"
+            v-bind:key="index"
+          >
+            <nuxt-link
+              :to="{ name: 'blog-slug', params: { slug: post.fields.slug } }"
+              class="wrapper"
+            >
+              <img
+                v-if="post.fields.featuredImage"
+                class="primary thumbnail"
+                :src="
+            post.fields.featuredImage.fields.file.url +
+              '?fit=scale&w=350&h=196&r=16'
+          "
+                :srcset="
+            `${
+              post.fields.featuredImage.fields.file.url
+            }?w=350&h=196&fit=fill 350w`
+          "
+                sizes="(min-width: 1024px) 400px, 100vw"
+              />
+              <div class="title">{{ post.fields.title }}</div>
+            </nuxt-link>
+          </div>
+          <!-- ... -->
+          <!-- <div class="w-1/2 lg:w-1/4 inline-block">9</div> -->
+        </div>
+        <div class="text-center">
+          <a href="/blog">See more</a>
+        </div>
+        <!-- <div class="center-content container mb-64">
           <div v-for="(post, index) in posts" v-bind:key="index">
             <nuxt-link
               :to="{ name: 'blog-slug', params: { slug: post.fields.slug } }"
@@ -17,7 +49,7 @@
               v-if="[0, 1, randomPostIndex].includes(index)"
             >{{ post.fields.title }}</nuxt-link>
           </div>
-        </div>
+        </div>-->
       </div>
     </div>
   </section>
@@ -33,7 +65,23 @@ export default {
     randomPostIndex: function() {
       // random number between 1 and posts.length
       return Math.floor(Math.random() * this.posts.length + 1);
+    },
+    recentPosts: function() {
+      return this.posts.slice(0, 4);
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+@import "tailwindcss";
+
+img {
+  width: 200px;
+  margin: 0 auto;
+}
+
+.title {
+  // height: 200px;
+}
+</style>
