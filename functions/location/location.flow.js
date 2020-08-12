af3c8c6ebf93493cb3b6d7eb6fc568560023b1df
tim <timmyg13@gmail.com>
2020-08-12 12:55:53 -0400

    message

--- functions/location/location.flow.js
+++ functions/location/location.flow.js
@@ -1331,10 +1331,11 @@ module.exports.slackSlashLocationsSearch = RavenLambdaWrapper.handler(Raven, asy
       }
     });
   });
-  console.log(responseText);
-  const response = respond(200, { text: responseText });
-  // respond.body = responseText;
-  return response;
+  // console.log(responseText);
+  return respond(200, responseText);
+  // const response = respond(200, { text: responseText });
+  // // respond.body = responseText;
+  // return response;
 });
 
 function buildAirtableNowShowing(location: Venue) {
