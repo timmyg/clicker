f477ec5402f96e29f351ec032dff4a4182c978e8
tim <timmyg13@gmail.com>
2020-08-21 10:49:17 -0400

    possible fix

--- web/app/components/GameMini.vue
+++ web/app/components/GameMini.vue
@@ -4,20 +4,18 @@
       <template v-if="game.broadcast">
         <div class="text-center text-xs">
           <div class="status">
-            <span class="tag" v-if="isInPast()"
-              >Now Showing on {{ getChannel() }}</span
-            >
-            <span class="tag" v-else
-              >Autotuning to
+            <span class="tag" v-if="isInPast()">Now Showing on {{ getChannel() }}</span>
+            <span class="tag" v-else>
+              Autotuning to
               <span class="brand-font">{{ getChannel() }}</span> in
-              <span class="brand-font">{{ timeRemaining }}</span></span
-            >
+              <span class="brand-font">{{ timeRemaining }}</span>
+            </span>
           </div>
         </div>
         <div class="flex teams-wrapper">
           <div class="w-full">
             <div class="team flex flex-row mb-2">
-              <template v-if="!!game.away">
+              <div v-if="!!game.away">
                 <div class="w-1/4"></div>
                 <div
                   v-for="team in getTeams()"
@@ -28,19 +26,18 @@
                     <img :src="team.logo" class="logo" />
                   </div>
                 </div>
-              </template>
-              <template v-else>
-                <div class="w-full flex flex-wrap justify-center">
-                  <div class="flex justify-center">
-                    <img :src="game.img" class="logo" />
-                  </div>
-                </div>
-              </template>
+              </div>
             </div>
           </div>
         </div>
       </template>
-      <template v-else> </template>
+      <template v-else>
+        <div class="w-full flex flex-wrap justify-center">
+          <div class="flex justify-center">
+            <img :src="game.img" class="logo" />
+          </div>
+        </div>
+      </template>
     </div>
   </div>
 </template>
