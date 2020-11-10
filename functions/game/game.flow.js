cc45bbed3649ae6eedbec31ad1bb962c88ef094a
tim <timmyg13@gmail.com>
2020-11-09 20:48:50 -0500

    games dependency 2

--- functions/game/game.flow.js
+++ functions/game/game.flow.js
@@ -260,15 +260,11 @@ function isBlowout(game: Game): boolean {
 }
 
 module.exports.syncActiveAirtable = RavenLambdaWrapper.handler(Raven, async event => {
-  // pull latest from AN
   const timeToPull = moment()
     .subtract(12, 'hours')
     .toDate();
   const allEvents = await pullFromActionNetwork([timeToPull]);
-  const ipAndCompletedGames = getInProgressAndCompletedGames(allEvents);
-  // get from airtable Games table where:
-  //  "waitOnGamesLink" or "controlCenterLink" are not null
-  //  isOver is false
+  const ipAndCompletedGames = [allEvents.find(e => e.id === 85412)];
   const base = new Airtable({ apiKey: process.env.airtableKey }).base(process.env.airtableBase);
   const airtableGamesName = 'Games';
   const allExistingGames = await base(airtableGamesName)
@@ -284,20 +280,35 @@ module.exports.syncActiveAirtable = RavenLambdaWrapper.handler(Raven, async even
       `,
     })
     .all();
-  const updates = [];
-  allExistingGames.forEach(game => {
+  const updateGames = [];
+  for (const game of allExistingGames) {
+    console.log({ game });
     const gameId = game.fields.id;
+    const gameRecordId = game.id;
     const gameScore = ipAndCompletedGames.find(g => g.id === gameId);
+    console.log({ gameScore });
     if (gameScore) {
-      game.isOver = gameScore.status === 'complete';
-      game.description = gameScore.scoreboard && gameScore.scoreboard.display;
-      updates.push(game);
+      const updatedGame = {
+        id: gameRecordId,
+        fields: {
+          isOver: gameScore.status === 'complete',
+          description: gameScore.status_display,
+        },
+      };
+      updateGames.push(updatedGame);
     }
-  });
-  if (updates.length) {
-    await Promise.all(base(airtableGamesName).update(updates));
   }
-  return respond();
+  const promises = [];
+  while (!!updateGames.length) {
+    try {
+      const slice = updateGames.splice(0, 10);
+      promises.push(base(airtableGamesName).update(slice));
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  await Promise.all(promises);
+  return respond(200);
 });
 
 module.exports.syncActive = RavenLambdaWrapper.handler(Raven, async event => {
@@ -658,8 +669,8 @@ async function pullFromActionNetwork(dates: Date[]) {
       requests.push(request);
     }
   }
-  console.log('requests:');
-  console.log(require('util').inspect(requests));
+  // console.log('requests:');
+  // console.log(require('util').inspect(requests));
   const responses = await Promise.all(requests);
   // console.log('responses[0]', responses[0].config.params);
   // console.log('responses[1]', responses[1].config.params);
