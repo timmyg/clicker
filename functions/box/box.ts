99becd48d62887e4fc79faa1ae720c77dffb35c4
tim <timmyg13@gmail.com>
2020-12-09 08:00:45 -0500

    fix source

--- functions/box/box.ts
+++ functions/box/box.ts
@@ -11,7 +11,7 @@ export const health = RavenLambdaWrapper.handler(Raven, async event => {
 
 export const nodeFunction = RavenLambdaWrapper.handler(Raven, async event => {
   console.log({event});
-  console.log(event.source.live.channel);
+  console.log(event.source.channel);
   return "hello world from node"
 });
 
