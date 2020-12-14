811867732e3fbcb7d70cffe5e3be72d3d95b20c0
tim <timmyg13@gmail.com>
2020-12-14 06:52:43 -0500

    gql

--- functions/box/box.ts
+++ functions/box/box.ts
@@ -9,36 +9,6 @@ export const health = RavenLambdaWrapper.handler(Raven, async event => {
   return respond(200, { vals });
 });
 
-export const fetchBoxProgram = RavenLambdaWrapper.handler(Raven, async event => {
-  const { channel, channelMinor } = event.source;
-  // TODO need region
-  const region = 'cincinnati';
-
-  if (channel) {
-    const queryParams = { channel, channelMinor, region };
-    console.log({ queryParams });
-    const programResult = await new Invoke()
-      .service('program')
-      .name('get')
-      .queryParams(queryParams)
-      .go();
-    return programResult && programResult.data;
-  }
-});
-
-export const fetchBoxProgramGame = RavenLambdaWrapper.handler(Raven, async event => {
-  console.log(event.source);
-  const { gameId } = event.source;
-  if (gameId) {
-    const result = await new Invoke()
-      .service('game')
-      .name('get')
-      .pathParams({ id: gameId })
-      .go();
-    return result && result.data;
-  }
-});
-
 export const remove = RavenLambdaWrapper.handler(Raven, async event => {
   const { locationId, boxId } = getPathParameters(event);
   const graphqlClient = getGraphqlClient();
