62f0f53eac25a4524e89a68ca497f10b2a47b4ab
tim <timmyg13@gmail.com>
2020-12-13 22:18:08 -0500

    gql

--- functions/graphql/graphql.ts
+++ functions/graphql/graphql.ts
@@ -7,3 +7,33 @@ require('cross-fetch/polyfill');
 export const health = RavenLambdaWrapper.handler(Raven, async event => {
   return respond(200, 'ok');
 });
+
+export const fetchBoxProgram = RavenLambdaWrapper.handler(Raven, async event => {
+  const { channel, channelMinor } = event.source;
+  // TODO need region
+  const region = 'cincinnati';
+
+  if (channel) {
+    const queryParams = { channel, channelMinor, region };
+    console.log({ queryParams });
+    const programResult = await new Invoke()
+      .service('program')
+      .name('get')
+      .queryParams(queryParams)
+      .go();
+    return programResult && programResult.data;
+  }
+});
+
+export const fetchBoxProgramGame = RavenLambdaWrapper.handler(Raven, async event => {
+  console.log(event.source);
+  const { gameId } = event.source;
+  if (gameId) {
+    const result = await new Invoke()
+      .service('game')
+      .name('get')
+      .pathParams({ id: gameId })
+      .go();
+    return result && result.data;
+  }
+});
