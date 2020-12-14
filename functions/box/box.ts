70a428449afbd0d7a25a2d8e2f80b04fa2339812
tim <timmyg13@gmail.com>
2020-12-14 11:07:11 -0500

    [skip e2e] fix box

--- functions/box/box.ts
+++ functions/box/box.ts
@@ -44,191 +44,177 @@ export const get = RavenLambdaWrapper.handler(Raven, async event => {
   return respond(200, result.data);
 });
 
-export const remove = RavenLambdaWrapper.handler(Raven, async event => {
-  const { locationId, boxId } = getPathParameters(event);
-  const graphqlClient = getGraphqlClient();
+// export const remove = RavenLambdaWrapper.handler(Raven, async event => {
+//   const { locationId, boxId } = getPathParameters(event);
+//   const graphqlClient = getGraphqlClient();
 
-  const mutation = gql(
-    `mutation deleteBox($id: ID!, $locationId: String!){
-      deleteBox(id: $id, locationId: $locationId){
-        id
-      }
-    }`,
-  );
-  const gqlMutation = graphqlClient.mutate({
-    mutation,
-    variables: {
-      locationId,
-      id: boxId,
-    },
-  });
-  const result = await gqlMutation;
-  return respond(200, result);
-});
+//   const mutation = gql(
+//     `mutation deleteBox($id: ID!, $locationId: String!){
+//       deleteBox(id: $id, locationId: $locationId){
+//         id
+//       }
+//     }`,
+//   );
+//   const gqlMutation = graphqlClient.mutate({
+//     mutation,
+//     variables: {
+//       locationId,
+//       id: boxId,
+//     },
+//   });
+//   const result = await gqlMutation;
+//   return respond(200, result);
+// });
 
-export const create = RavenLambdaWrapper.handler(Raven, async event => {
-  const { locationId } = getPathParameters(event);
-  const boxes = getBody(event);
-  const graphqlClient = getGraphqlClient();
+// export const create = RavenLambdaWrapper.handler(Raven, async event => {
+//   const { locationId } = getPathParameters(event);
+//   const boxes = getBody(event);
+//   const graphqlClient = getGraphqlClient();
 
-  const boxesCreated = [];
-  for (let newBox of boxes) {
-    const mutation = gql(
-      `mutation addBox($id: ID!, $locationId: String!, $label: String, $zone: String, $info: BoxInfoInput!, $configuration: BoxConfigurationInput!){
-        addBox(id: $id, locationId: $locationId, label: $label, zone: $zone, info: $info, configuration: $configuration){
-          id
-          locationId
-        }
-      }`,
-    );
-    console.time('create');
-    newBox.locationId = locationId;
-    newBox.id = uuid();
-    console.log({ newBox });
-    const gqlMutation = graphqlClient.mutate({
-      mutation,
-      variables: newBox,
-    });
-    console.timeEnd('create');
-    const { data } = await gqlMutation;
-    boxesCreated.push(data.addBox);
-  }
-  return respond(201, boxesCreated);
-});
+//   const boxesCreated = [];
+//   for (let newBox of boxes) {
+//     const mutation = gql(
+//       `mutation addBox($id: ID!, $locationId: String!, $label: String, $zone: String, $info: BoxInfoInput!, $configuration: BoxConfigurationInput!){
+//         addBox(id: $id, locationId: $locationId, label: $label, zone: $zone, info: $info, configuration: $configuration){
+//           id
+//           locationId
+//         }
+//       }`,
+//     );
+//     console.time('create');
+//     newBox.locationId = locationId;
+//     newBox.id = uuid();
+//     console.log({ newBox });
+//     const gqlMutation = graphqlClient.mutate({
+//       mutation,
+//       variables: newBox,
+//     });
+//     console.timeEnd('create');
+//     const { data } = await gqlMutation;
+//     boxesCreated.push(data.addBox);
+//   }
+//   return respond(201, boxesCreated);
+// });
 
-export const createDirectv = RavenLambdaWrapper.handler(Raven, async event => {
-  const { ip, boxes }: DirectvBoxRequest = getBody(event);
-  const { locationId } = getPathParameters(event);
+// export const createDirectv = RavenLambdaWrapper.handler(Raven, async event => {
+//   const { ip, boxes }: DirectvBoxRequest = getBody(event);
+//   const { locationId } = getPathParameters(event);
 
-  const newBox = {
-    id: uuid(),
-    locationId,
-    configuration: {
-      appActive: false,
-      automationActive: false,
-    },
-    info: {
-      ip,
-      clientAddress: boxes[0].clientAddr,
-      locationName: boxes[0].locationName,
-    },
-    label: Math.random()
-      .toString(36)
-      .substr(2, 2),
-    zone: Math.random()
-      .toString(36)
-      .substr(2, 2),
-    live: {},
-  };
-  console.log({ newBox });
+//   const newBox = {
+//     id: uuid(),
+//     locationId,
+//     configuration: {
+//       appActive: false,
+//       automationActive: false,
+//     },
+//     info: {
+//       ip,
+//       clientAddress: boxes[0].clientAddr,
+//       locationName: boxes[0].locationName,
+//     },
+//     label: Math.random()
+//       .toString(36)
+//       .substr(2, 2),
+//     zone: Math.random()
+//       .toString(36)
+//       .substr(2, 2),
+//     live: {},
+//   };
+//   console.log({ newBox });
 
-  const result = await new Invoke()
-    .service('box')
-    .name('create')
-    .pathParams({ locationId })
-    .body([newBox])
-    .go();
+//   const result = await new Invoke()
+//     .service('box')
+//     .name('create')
+//     .pathParams({ locationId })
+//     .body([newBox])
+//     .go();
 
-  return respond(200, result.data.addBox);
-});
+//   return respond(200, result.data.addBox);
+// });
 
-export const updateLive = RavenLambdaWrapper.handler(Raven, async event => {
-  const { locationId, boxId } = getPathParameters(event);
-  const live = getBody(event);
-  const graphqlClient = getGraphqlClient();
+// export const updateLive = RavenLambdaWrapper.handler(Raven, async event => {
+//   const { locationId, boxId } = getPathParameters(event);
+//   const live = getBody(event);
+//   const graphqlClient = getGraphqlClient();
 
-  // https://github.com/serverless/serverless-graphql/blob/master/app-backend/appsync
-  const mutation = gql(
-    `mutation updateBoxLive($id: ID!, $locationId: String!, $live: BoxLiveInput!){
-      updateBoxLive(id: $id, locationId: $locationId, live: $live){
-        id
-        locationId
-        live {
-          channel
-        }
-      }
-    }`,
-  );
-  console.time('create');
+//   // https://github.com/serverless/serverless-graphql/blob/master/app-backend/appsync
+//   const mutation = gql(
+//     `mutation updateBoxLive($id: ID!, $locationId: String!, $live: BoxLiveInput!){
+//       updateBoxLive(id: $id, locationId: $locationId, live: $live){
+//         id
+//         locationId
+//         live {
+//           channel
+//         }
+//       }
+//     }`,
+//   );
+//   console.time('create');
 
-  const variables = {
-    live: {
-      channel: live.channel,
-      channelMinor: live.channelMinor,
-      channelChangeAt: live.channelChangeAt,
-      channelChangeSource: live.channelChangeSource,
-      lockedProgrammingIds: live.lockedProgrammingIds,
-      lockedUntil: live.lockedUntil,
-    },
-    id: boxId,
-    locationId,
-  };
-  console.log({ variables });
-  const gqlMutation = graphqlClient.mutate({
-    mutation,
-    variables,
-  });
-  console.timeEnd('create');
-  const result = await gqlMutation;
-  return respond(200, result.data.addBox);
-});
+//   const variables = {
+//     live: {
+//       channel: live.channel,
+//       channelMinor: live.channelMinor,
+//       channelChangeAt: live.channelChangeAt,
+//       channelChangeSource: live.channelChangeSource,
+//       lockedProgrammingIds: live.lockedProgrammingIds,
+//       lockedUntil: live.lockedUntil,
+//     },
+//     id: boxId,
+//     locationId,
+//   };
+//   console.log({ variables });
+//   const gqlMutation = graphqlClient.mutate({
+//     mutation,
+//     variables,
+//   });
+//   console.timeEnd('create');
+//   const result = await gqlMutation;
+//   return respond(200, result.data.addBox);
+// });
 
-export const getAll = RavenLambdaWrapper.handler(Raven, async event => {
-  const { locationId } = getPathParameters(event);
-  const graphqlClient = getGraphqlClient();
-  const query = gql(`
-    query boxes($locationId: String!)
-      {
-        boxes(locationId: $locationId) {
-          id
-          configuration {
-            automationActive
-            appActive
-          }
-          info {
-            ip
-            clientAddress
-          }
-          label
-          zone
-        }
-      }
-  `);
-  const gqlQuery = graphqlClient.query({
-    query,
-    variables: {
-      locationId,
-    },
-  });
-  console.log({ locationId });
-  console.time('query');
-  const { data } = await gqlQuery;
-  console.timeEnd('query');
-  return respond(200, data);
-});
+// export const getAll = RavenLambdaWrapper.handler(Raven, async event => {
+//   const { locationId } = getPathParameters(event);
+//   const graphqlClient = getGraphqlClient();
+//   const query = gql(`
+//     query boxes($locationId: String!)
+//       {
+//         boxes(locationId: $locationId) {
+//           id
+//           configuration {
+//             automationActive
+//             appActive
+//           }
+//           info {
+//             ip
+//             clientAddress
+//           }
+//           label
+//           zone
+//         }
+//       }
+//   `);
+//   const gqlQuery = graphqlClient.query({
+//     query,
+//     variables: {
+//       locationId,
+//     },
+//   });
+//   console.log({ locationId });
+//   console.time('query');
+//   const { data } = await gqlQuery;
+//   console.timeEnd('query');
+//   return respond(200, data);
+// });
 
-function getGraphqlClient() {
-  const { graphqlApiUrl, graphqlApiKey, region } = process.env;
+// class DirectvBox {
+//   major: number;
+//   minor: number;
+//   clientAddr: string;
+//   locationName: string;
+// }
 
-  return new appsync.AWSAppSyncClient({
-    url: graphqlApiUrl,
-    region: region,
-    auth: {
-      type: appsync.AUTH_TYPE.API_KEY,
-      apiKey: graphqlApiKey,
-    },
-    disableOffline: true,
-  });
-}
-
-class DirectvBox {
-  major: number;
-  minor: number;
-  clientAddr: string;
-  locationName: string;
-}
-
-class DirectvBoxRequest {
-  boxes: DirectvBox[];
-  ip: string;
-}
+// class DirectvBoxRequest {
+//   boxes: DirectvBox[];
+//   ip: string;
+// }
