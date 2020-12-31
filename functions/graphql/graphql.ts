7c717151c619abf3e65bf1dca9d82db862ff58ab
tim <timmyg13@gmail.com>
2020-12-31 08:51:34 -0500

    cleanup

--- functions/graphql/graphql.ts
+++ functions/graphql/graphql.ts
@@ -1,7 +1,5 @@
 import { getBody, respond, Raven, RavenLambdaWrapper, Invoke } from 'serverless-helpers';
 const appsync = require('aws-appsync');
-const gql = require('graphql-tag');
-const uuid = require('uuid/v1');
 require('cross-fetch/polyfill');
 
 export const health = RavenLambdaWrapper.handler(Raven, async event => {
