31e7d3c043e095a9ec729bd1d75c5e78a5900c98
tim <timmyg13@gmail.com>
2021-01-11 08:33:53 -0500

    type to class

--- functions/program/program.ts
+++ functions/program/program.ts
@@ -26,12 +26,12 @@ export const getByLocation = RavenLambdaWrapper.handler(Raven, async event => {
   return respond(200, 'hello!');
 });
 
-type region = {
+class region {
   id: string;
   name: string;
   defaultZip: string;
   localChannels: number[];
-};
+}
 
 // duplicated!
 const allPackages: any = [
